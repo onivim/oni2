639d2b5c9d11386e3f9f66832c503955a53894ea
bryphe <bryphe@outlook.com>
2019-02-28 13:08:00 -0800

    Initial grammar loading

--- src/textmate_service/src/index.ts
+++ src/textmate_service/src/index.ts
@@ -4,18 +4,56 @@
  * Entry for the external textmate tokenizer service
  */
 
-
+import * as fs from "fs";
 import * as rpc from "vscode-jsonrpc";
+import * as vsctm from "vscode-textmate";
 
 let connection = rpc.createMessageConnection(new rpc.StreamMessageReader(process.stdin), new rpc.StreamMessageWriter(process.stdout));
 
-let initializeNotification = new rpc.NotificationType<string, void>('initialize');
+interface ITextmateInitData {
+    [scope: string]: string;
+};
+
+let initializeNotification = new rpc.NotificationType<ITextmateInitData, void>('initialize');
 let initializedNotification = new rpc.NotificationType<string, void>('initialized');
 let exitNotification = new rpc.NotificationType<string, void>('exit');
 
+let textmateGrammarPreloadNotification = new rpc.NotificationType<string, string>('textmate/preloadScope');
+let textmateGrammarLoadedNotification = new rpc.NotificationType<string, void>('textmate/scopeLoaded');
+
+let grammarPaths: ITextmateInitData = {};
+
+const registry = new vsctm.Registry({
+        loadGrammar: function (scopeName) {
+            var path = grammarPaths[scopeName];
+            if (path) {
+                return new Promise((c,e) => {
+                    fs.readFile(path, (error, content) => {
+                        if (error) {
+                            e(error);
+                        } else {
+                            var rawGrammar = vsctm.parseRawGrammar(content.toString(), path);
+                            connection.sendNotification("textmate/scopeLoaded", scopeName);
+                            c(rawGrammar);
+                        }
+                    });
+                });
+            } else  {
+                return <any>null;
+            }
+        }
+    });
+
 connection.listen();
 
-connection.onNotification(initializeNotification, () => {
+connection.onNotification(textmateGrammarPreloadNotification, (scope) => {
+    registry.loadGrammar(scope);
+});
+
+
+connection.onNotification(initializeNotification, (paths: ITextmateInitData) => {
+    console.error("LOADING: " + JSON.stringify(paths));
+    grammarPaths = paths;
     connection.sendNotification(initializedNotification, {});  
 });
 
--- test/editor/Core/TextmateTokenizerTests.re
+++ test/editor/Core/TextmateTokenizerTests.re
@@ -3,7 +3,7 @@ open TestFramework;
 
 open Reason_jsonrpc;
 
-describe("Textmate Service", ({test, _}) =>
+describe("Textmate Service", ({test, _}) => {
   test("receive init message", ({expect}) =>
     Helpers.repeat(() => {
       let setup = Setup.init();
@@ -54,6 +54,46 @@ describe("Textmate Service", ({test, _}) =>
       | _ =>
         expect.string("Expected WEXITED").toEqual("Got different exit state")
       };
-    })
-  )
-);
+    }));
+
+  test("load grammar / scope", ({expect}) => {
+       let setup = Setup.init(); 
+    let proc = NodeProcess.start(setup, setup.textmateServicePath);
+
+
+    let gotScopeLoadedMessage = ref(false);
+
+      let onNotification = (n: Notification.t, _) =>
+        switch ((n.method, n.params)) {
+        | ("textmate/scopeLoaded", `String("source.reason")) => {
+           gotScopeLoadedMessage := true; 
+        }
+        | _ => prerr_endline("Unrecognized message!")
+        };
+
+      let onRequest = (_, _) => Ok(Yojson.Safe.from_string("{}"));
+
+      let onClose = () => ();
+
+      let rpc =
+        Rpc.start(
+          ~onNotification,
+          ~onRequest,
+          ~onClose,
+          proc.stdout,
+          proc.stdin,
+        );
+
+      Rpc.sendNotification(rpc, "initialize", `Assoc([
+        ("source.reason", `String("D:/oni2/extensions/vscode-reasonml/syntaxes/reason.json"))
+      ]));
+
+      Rpc.sendNotification(rpc, "textmate/preloadScope", `String("source.reason"));
+
+      Oni_Core.Utility.waitForCondition(() => {
+        Rpc.pump(rpc);
+        gotScopeLoadedMessage^
+      });
+      expect.bool(gotScopeLoadedMessage^).toBe(true);
+  });
+});
