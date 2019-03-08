959fa351de010b2c34c86ad5bb67a6e0a7fbe945
bryphe <bryphe@outlook.com>
2019-03-08 09:22:00 -0800

    Continue building scripts

--- scripts/release.js
+++ scripts/release.js
@@ -6,13 +6,18 @@ console.log("Working directory: " + process.cwd());
 const releaseDirectory = path.join(process.cwd(), "_release");
 const platformReleaseDirectory = path.join(releaseDirectory, process.platform);
 
+const extensionsSourceDirectory = path.join(process.cwd(), "extensions");
+const extensionsDestDirectory = path.join(platformReleaseDirectory, "extensions");
+
 console.log("Creating directory: " + platformReleaseDirectory);
 
 fs.mkdirpSync(platformReleaseDirectory);
 
 // Copy the cur__bin over
-
 let curBin = process.env["cur__bin"];
 console.log("Bin folder: " + curBin);
 
 fs.copySync(curBin, platformReleaseDirectory);
+
+// Copy extensions over
+fs.copySync(extensionsSourceDirectory, extensionsDestDirectory);
