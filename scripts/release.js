86bf5ceb85d2f863da8e5099fc8c7fcc8186a935
Bryan Phelps <bryphe@outlook.com>
2019-07-03 07:51:06 -0700

    Release script updates

--- scripts/release.js
+++ scripts/release.js
@@ -2,28 +2,57 @@ const cp = require("child_process");
 const fs = require("fs-extra");
 const path = require("path");
 
+let curBin = process.env["cur__bin"];
+console.log("Bin folder: " + curBin);
 console.log("Working directory: " + process.cwd());
 
 const releaseDirectory = path.join(process.cwd(), "_release");
-const platformReleaseDirectory = path.join(releaseDirectory, process.platform);
 
 const extensionsSourceDirectory = path.join(process.cwd(), "extensions");
-const extensionsDestDirectory = path.join(platformReleaseDirectory, "extensions");
+// const extensionsDestDirectory = path.join(platformReleaseDirectory, "extensions");
 
-console.log("Creating directory: " + platformReleaseDirectory);
+const copy = (source, dest) => {
+    console.log(`Copying from ${source} to ${dest}`);
+     if (process.platform == "darwin") {
+        shell(`cp -r "${source}" "${dest}"`)
+     } else {
+        fs.copySync(source, dest);
+     }
+    console.log("Successfully copied.");
+};
 
-fs.mkdirpSync(platformReleaseDirectory);
+const shell = (cmd) => {
+    console.log(`[shell] ${cmd}`);
+    const out = cp.execSync(cmd);
+    console.log(`[shell - output]: ${out.toString("utf8")}`);
+};
 
-// Copy the cur__bin over
-let curBin = process.env["cur__bin"];
-console.log("Bin folder: " + curBin);
+if (process.platform == "darwin") {
 
-fs.copySync(curBin, platformReleaseDirectory);
+  const executables = [
+    "Oni2",
+    "Oni2_editor",
+  ];
 
-// Copy extensions over
-fs.copySync(extensionsSourceDirectory, extensionsDestDirectory);
+  const appDirectory = path.join(releaseDirectory, "Onivim2.App");
+  const contentsDirectory = path.join(appDirectory, "Contents");
+  const binaryDirectory = path.join(contentsDirectory, "MacOS");
+  const libsDirectory = path.join(contentsDirectory, "libs");
+  const extensionsDestDirectory = path.join(contentsDirectory, "extensions");
 
-if (process.platform == 'darwin') {
-	let out = cp.execSync("dylibbundler", ["--help"]).toString("utf8");
-	console.log("dylibbundler output: " + out);
+  fs.mkdirpSync(libsDirectory);
+  fs.mkdirpSync(extensionsDestDirectory);
+
+  // Copy bins over
+  copy(curBin, binaryDirectory);
+  copy(extensionsSourceDirectory, extensionsDestDirectory);
+
+  shell(`dylibbundler -b -x "${path.join(binaryDirectory, "Oni2_editor")}" -d "${libsDirectory}" -cd`);
+} else {
+  const platformReleaseDirectory = path.join(releaseDirectory, process.platform);
+  const extensionsDestDirectory = path.join(releaseDirectory, "extensions");
+  fs.mkdirpSync(platformReleaseDirectory);
+
+  fs.copySync(curBin, binaryDirectory, { deference: true});
+  fs.copySync(extensionsSourceDirectory, extensionsDestDirectory, {deference: true});
 }
