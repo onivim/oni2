ef05345b9a0035af92258968b29a827b938610da
Bryan Phelps <bryphe@outlook.com>
2019-07-11 16:49:36 -0700

    Copy desktop file

--- scripts/linux/package-linux.sh
+++ scripts/linux/package-linux.sh
@@ -11,6 +11,11 @@ cp -r $cur__bin _release/linux
 wget -O _staging/linuxdeploy-x86_64.AppImage https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 chmod +x _staging/linuxdeploy-x86_64.AppImage
 
-./_staging/linuxdeploy-x86_64.AppImage -e _release/linux/bin/Oni2_editor --appdir _release/linux
+cp scripts/linux/Onivim2.desktop _release/linux/Onivim2.desktop
+cp assets/images/logo.png _release/linux/Onivim2.png
 
+./_staging/linuxdeploy-x86_64.AppImage -e _release/linux/bin/Oni2_editor --appdir _release/Onivim2.AppDir -d _release/linux/Onivim2.desktop -i _release/linux/Onivim2.png
+
+cp _release/linux/bin/*.* _release/Onivim2.AppDir/usr/bin
+cp _release/linux/bin/Oni2 _release/Onivim2.AppDir/usr/bin/Oni2
 
