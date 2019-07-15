3bd904612e657350f8756451763e7652137ae96c
bryphe <bryphe@outrunlabs.com>
2019-07-15 11:00:52 -0700

    Add ls for linux / osx

--- scripts/osx/validate-release.sh
+++ scripts/osx/validate-release.sh
@@ -4,4 +4,6 @@ ls $SYSTEM_ARTIFACTSDIRECTORY
 
 tar -xzf $SYSTEM_ARTIFACTSDIRECTORY/Onivim2.tar.gz _unpacked/.
 
+ls _unpacked
+
 ONI2_DEBUG=1 ./_unpacked/Onivim2.App/Contents/MacOS/Oni2 -f --checkhealth
