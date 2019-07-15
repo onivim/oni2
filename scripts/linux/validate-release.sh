85c17f0ecb040a42dcedfb0e02c548bf91bb1362
bryphe <bryphe@outrunlabs.com>
2019-07-15 10:50:51 -0700

    Add validate-release scripts

--- scripts/linux/validate-release.sh
+++ scripts/linux/validate-release.sh
@@ -1,5 +1,7 @@
 mkdir -p _unpacked
 
+ls $SYSTEM_ARTIFACTSDIRECTORY
+
 tar -xzf $SYSTEM_ARTIFACTSDIRECTORY/Onivim2.tar.gz _unpacked/.
 
 ONI2_DEBUG=1 ./_unpacked/Onivim2.AppDir/usr/bin/Oni2 -f --checkhealth
