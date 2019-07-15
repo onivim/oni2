60d320cf1d06472c6decce6c6ff02a3e94cc9835
bryphe <bryphe@outrunlabs.com>
2019-07-15 10:40:38 -0700

    Add windows release script

---
+++ scripts/windows/validate-release.ps1
@@ -0,0 +1,7 @@
+mkdir -p _unpacked
+
+Expand-Archive -Path $env:SYSTEM_ARTIFACTSDIRECTORY/Onivim2.zip -DestinationPath _unpacked/
+
+ls _unpacked
+$env:ONI2_DEBUG=1
+./_unpacked/win32/Oni2.exe -f --checkhealth
