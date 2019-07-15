23a03bb99637395c2ae5aec0584661493f9d732d
bryphe <bryphe@outrunlabs.com>
2019-07-15 11:49:38 -0700

    More script fixes

--- scripts/windows/validate-release.ps1
+++ scripts/windows/validate-release.ps1
@@ -1,6 +1,9 @@
 mkdir -p _unpacked
 
-Expand-Archive -Path $env:SYSTEM_ARTIFACTSDIRECTORY/Onivim2.zip -DestinationPath _unpacked/
+ls $env:SYSTEM_ARTIFACTSDIRECTORY
+ls $env:SYSTEM_ARTIFACTSDIRECTORY/Release_Windows
+
+Expand-Archive -Path $env:SYSTEM_ARTIFACTSDIRECTORY/Release_Windows/Onivim2.zip -DestinationPath _unpacked/
 
 ls _unpacked
 $env:ONI2_DEBUG=1
