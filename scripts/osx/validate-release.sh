50ce1e4c5bc84ec7ce4e3c5ed81969b3859d47ee
Bryan Phelps <bryphe@outlook.com>
2019-07-17 07:03:38 -0700

    Add script to validate DMG

--- scripts/osx/validate-release.sh
+++ scripts/osx/validate-release.sh
@@ -1,3 +1,4 @@
+echo "** Validating raw app **"
 mkdir -p _unpacked
 
 ls $SYSTEM_ARTIFACTSDIRECTORY
@@ -7,3 +8,15 @@ tar -xzf $SYSTEM_ARTIFACTSDIRECTORY/Release_Darwin/Onivim2.tar.gz -C _unpacked
 ls _unpacked
 
 ONI2_DEBUG=1 ./_unpacked/Onivim2.App/Contents/MacOS/Oni2 -f --checkhealth
+
+echo "** Validating DMG **"
+rm -rf _unpacked
+mkdir _unpacked
+sudo hdiutil attach $SYSTEM_ARTIFACTSDIRECTORY/Onivim2.dmg
+cp -rf "/Volumes/Onivim 2"/*.App _unpacked
+sudo hdiutil detach "/Volumes/Onivim 2"
+ONI2_DEBUG=1 ./_unpacked/Onivim2.App/Contents/MacOS/Oni2 -f --checkhealth
+
+
+
+
