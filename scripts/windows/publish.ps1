6602872f04d3a95a3d996855f267a87357a1e87b
bryphe <bryphe@outrunlabs.com>
2019-07-17 07:00:57 -0700

    Fix output path

--- scripts/windows/publish.ps1
+++ scripts/windows/publish.ps1
@@ -5,4 +5,4 @@ Compress-Archive -Path _release/win32 -DestinationPath _publish/Onivim2.zip
 esy create-win-setup
 
 npm install -g innosetup-compiler
-innosetup-compiler _release/setup.iss --O=_publish/Onivim2-x64.exe
+innosetup-compiler _release/setup.iss --O=_publish
