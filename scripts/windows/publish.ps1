583be66b4c60cf74ea59bfde6b89a47ab74d029a
bryphe <bryphe@outrunlabs.com>
2019-07-17 06:29:25 -0700

    Create setup template file

--- scripts/windows/publish.ps1
+++ scripts/windows/publish.ps1
@@ -2,6 +2,7 @@ mkdir -p _publish
 
 Compress-Archive -Path _release/win32 -DestinationPath _publish/Onivim2.zip
 
-npm install -g innosetup-compiler
+esy create-win-setup
 
+npm install -g innosetup-compiler
 innosetup-compiler _release/setup.iss --O=_publish/Onivim2-x64.exe
