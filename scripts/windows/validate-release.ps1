6b716f2abe45df39d705eb67da2bb6318b2c1f31
bryphe <bryphe@outrunlabs.com>
2019-07-17 10:24:23 -0700

    Try another path

--- scripts/windows/validate-release.ps1
+++ scripts/windows/validate-release.ps1
@@ -13,12 +13,15 @@ Write-Host "** Validating .exe installer **"
 rm -r _unpacked
 mkdir _unpacked
 cp $env:SYSTEM_ARTIFACTSDIRECTORY/Release_Windows/Onivim2-0.2.0-win.exe _unpacked/Onivim2-0.2.0-win.exe
-_unpacked/Onivim2-0.2.0-win.exe /silent /verysilent /sp /suppressmsgboxes /norestart /Dir="D:/Onivim2"
+_unpacked/Onivim2-0.2.0-win.exe /silent /verysilent /sp /suppressmsgboxes /norestart /Dir="D:/a/1/Onivim2"
 Write-Host "Ran installer!"
 
 ls .
 ls _unpacked
 
-ls D:/Onivim2
-D:/Onivim2/Oni2.exe -f --checkhealth
+ls D:/
+ls D:/a/1
+ls D:/a/1/Onivim2
+
+D:/a/1/Onivim2/Oni2.exe -f --checkhealth
 
