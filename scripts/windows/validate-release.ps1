367d14eb0b6384de2ca880045a70acbca2f729f2
bryphe <bryphe@outrunlabs.com>
2019-07-17 07:10:31 -0700

    Validate windows installer

--- scripts/windows/validate-release.ps1
+++ scripts/windows/validate-release.ps1
@@ -3,8 +3,17 @@ mkdir -p _unpacked
 ls $env:SYSTEM_ARTIFACTSDIRECTORY
 ls $env:SYSTEM_ARTIFACTSDIRECTORY/Release_Windows
 
+Write-Host "** Validating .zip package **"
 Expand-Archive -Path $env:SYSTEM_ARTIFACTSDIRECTORY/Release_Windows/Onivim2.zip -DestinationPath _unpacked/
-
 ls _unpacked
 $env:ONI2_DEBUG=1
 ./_unpacked/win32/Oni2.exe -f --checkhealth
+
+Write-Host "** Validating .exe installer **"
+rm -rf _unpacked
+mkdir _unpacked
+$env:SYSTEM_ARTIFACTSDIRECTORY/Release_Windows/Onivim2-0.2.0-win.exe /silent /verysilent /sp /suppressmsgboxes /norestart /Dir="_unpacked/installed_app"
+
+ls _unpacked/installed_app
+./_unpacked/installed_app/win32/Oni2.exe -f --checkhealth
+
