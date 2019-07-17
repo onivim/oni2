a09d6d4083f58549b9b8031227037f80e0241f2b
Bryan Phelps <bryphe@outlook.com>
2019-07-17 07:14:40 -0700

    Fix paths

--- scripts/osx/validate-release.sh
+++ scripts/osx/validate-release.sh
@@ -2,6 +2,7 @@ echo "** Validating raw app **"
 mkdir -p _unpacked
 
 ls $SYSTEM_ARTIFACTSDIRECTORY
+ls $SYSTEM_ARTIFACTSDIRECTORY/Release_Darwin
 
 tar -xzf $SYSTEM_ARTIFACTSDIRECTORY/Release_Darwin/Onivim2.tar.gz -C _unpacked
 
@@ -12,7 +13,7 @@ ONI2_DEBUG=1 ./_unpacked/Onivim2.App/Contents/MacOS/Oni2 -f --checkhealth
 echo "** Validating DMG **"
 rm -rf _unpacked
 mkdir _unpacked
-sudo hdiutil attach $SYSTEM_ARTIFACTSDIRECTORY/Onivim2.dmg
+sudo hdiutil attach $SYSTEM_ARTIFACTSDIRECTORY/Release_Darwin/Onivim2.dmg
 cp -rf "/Volumes/Onivim 2"/*.App _unpacked
 sudo hdiutil detach "/Volumes/Onivim 2"
 ONI2_DEBUG=1 ./_unpacked/Onivim2.App/Contents/MacOS/Oni2 -f --checkhealth
