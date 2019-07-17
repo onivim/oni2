61b73a244d1f41fecad9f9bc5ecac71de988307c
Bryan Phelps <bryphe@outrunlabs.com>
2019-07-17 07:42:09 -0700

    Packaging - OSX: Verify DMG (#455)
    * Publish DMG
    
    * Fix paths
    
    * Add script to validate DMG
    
    * Remove extra empty lines
    
    * Fix paths

--- scripts/osx/publish.sh
+++ scripts/osx/publish.sh
@@ -1,3 +1,7 @@
+npm install -g appdmg
+
 mkdir -p _publish
 
 tar -C _release -cvzf _publish/Onivim2.tar.gz Onivim2.App
+
+appdmg _release/appdmg.json _publish/Onivim2.dmg
--- scripts/osx/validate-release.sh
+++ scripts/osx/validate-release.sh
@@ -1,9 +1,19 @@
+echo "** Validating raw app **"
 mkdir -p _unpacked
 
 ls $SYSTEM_ARTIFACTSDIRECTORY
+ls $SYSTEM_ARTIFACTSDIRECTORY/Release_Darwin
 
 tar -xzf $SYSTEM_ARTIFACTSDIRECTORY/Release_Darwin/Onivim2.tar.gz -C _unpacked
 
 ls _unpacked
 
+ONI2_DEBUG=1 ./_unpacked/Onivim2.App/Contents/MacOS/Oni2 -f --checkhealth
+
+echo "** Validating DMG **"
+rm -rf _unpacked
+mkdir _unpacked
+sudo hdiutil attach $SYSTEM_ARTIFACTSDIRECTORY/Release_Darwin/Onivim2.dmg
+cp -rf "/Volumes/Onivim 2"/*.App _unpacked
+sudo hdiutil detach "/Volumes/Onivim 2"
 ONI2_DEBUG=1 ./_unpacked/Onivim2.App/Contents/MacOS/Oni2 -f --checkhealth
