f39c8490b6f574835c858af0feb382207891d744
Bryan Phelps <bryphe@outrunlabs.com>
2019-07-17 06:27:51 -0700

    Packaging - Linux: Run --checkhealth against AppImage (#454)
    * Verify AppImage
    
    * Setup script

--- scripts/linux/validate-release.sh
+++ scripts/linux/validate-release.sh
@@ -1,3 +1,4 @@
+
 mkdir -p _unpacked
 
 ls $SYSTEM_ARTIFACTSDIRECTORY
@@ -7,3 +8,10 @@ tar -xzf $SYSTEM_ARTIFACTSDIRECTORY/Release_linux/Onivim2.tar.gz -C _unpacked
 ls _unpacked
 
 ONI2_DEBUG=1 ./_unpacked/Onivim2.AppDir/usr/bin/Oni2 -f --checkhealth
+
+cp $SYSTEM_ARTIFACTSDIRECTORY/Release_linux/Onivim2-x86_64.AppImage _unpacked/Onivim2-x86_64.AppImage
+
+ls _unpacked
+
+chmod +x _unpacked/Onivim2-x86_64.AppImage
+./_unpacked/Onivim2-x86_64.AppImage -f --checkhealth
