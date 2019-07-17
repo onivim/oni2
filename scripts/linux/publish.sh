42b2911130029b90fde936528408bc8c5e7c55ef
bryphe <bryphe@outrunlabs.com>
2019-07-16 20:17:29 -0700

    Verify AppImage

--- scripts/linux/publish.sh
+++ scripts/linux/publish.sh
@@ -1,3 +1,5 @@
 mkdir -p _publish
 
 tar -C _release -cvzf _publish/Onivim2.tar.gz Onivim2.AppDir
+
+cp _release/Onivim2-x86_64.AppImage _publish/Onivim2-x86_64.AppImage
--- scripts/linux/validate-release.sh
+++ scripts/linux/validate-release.sh
@@ -1,3 +1,4 @@
+
 mkdir -p _unpacked
 
 ls $SYSTEM_ARTIFACTSDIRECTORY
@@ -7,3 +8,7 @@ tar -xzf $SYSTEM_ARTIFACTSDIRECTORY/Release_linux/Onivim2.tar.gz -C _unpacked
 ls _unpacked
 
 ONI2_DEBUG=1 ./_unpacked/Onivim2.AppDir/usr/bin/Oni2 -f --checkhealth
+
+cp $SYSTEM_ARTIFACTSDIRECTORY/Onivim2-x86_64.AppImage Onivim2-x86_64.AppImage
+chmod +x Onivim2-x86_64.AppImage
+./Onivim2-x86_64.AppImage -f --checkhealth
