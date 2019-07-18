91bbf7157cf2d3324db51cb3e489b628fc9bea11
Bryan Phelps <bryphe@outrunlabs.com>
2019-07-17 20:06:27 -0700

    Packaging - Windows: Verify setup.exe (#456)
    * Generate setup exe
    
    * Create setup template file
    
    * Fix output path
    
    * Validate windows installer
    
    * Tweak install step
    
    * Remove -f parameter from rm for powershell
    
    * Fix path
    
    * Fix another missed name
    
    * Log install location
    
    * Try installing to root
    
    * Try another path
    
    * Add log
    
    * Tweak log path
    
    * More logging
    
    * Pipe to Out-Null to wait for installer to finish

--- scripts/windows/publish.ps1
+++ scripts/windows/publish.ps1
@@ -1,3 +1,8 @@
 mkdir -p _publish
 
 Compress-Archive -Path _release/win32 -DestinationPath _publish/Onivim2.zip
+
+esy create-win-setup
+
+npm install -g innosetup-compiler
+innosetup-compiler _release/setup.iss --O=_publish
--- scripts/windows/validate-release.ps1
+++ scripts/windows/validate-release.ps1
@@ -3,8 +3,29 @@ mkdir -p _unpacked
 ls $env:SYSTEM_ARTIFACTSDIRECTORY
 ls $env:SYSTEM_ARTIFACTSDIRECTORY/Release_Windows
 
+Write-Host "** Validating .zip package **"
 Expand-Archive -Path $env:SYSTEM_ARTIFACTSDIRECTORY/Release_Windows/Onivim2.zip -DestinationPath _unpacked/
-
 ls _unpacked
 $env:ONI2_DEBUG=1
 ./_unpacked/win32/Oni2.exe -f --checkhealth
+
+Write-Host "** Validating .exe installer **"
+rm -r _unpacked
+mkdir _unpacked
+cp $env:SYSTEM_ARTIFACTSDIRECTORY/Release_Windows/Onivim2-0.2.0-win.exe _unpacked/Onivim2-0.2.0-win.exe
+_unpacked/Onivim2-0.2.0-win.exe /silent /verysilent /sp /suppressmsgboxes /norestart /Dir="D:/a/1/s/Onivim2" /log="D:/a/1/s/install.log" | Out-Null
+
+Write-Host "Ran installer!"
+
+ls .
+ls D:/a/1/s
+ls _unpacked
+
+Get-Content -Path install.log
+
+ls D:/
+ls D:/a/1/s
+ls D:/a/1/s/Onivim2
+
+D:/a/1/s/Onivim2/Oni2.exe -f --checkhealth
+
