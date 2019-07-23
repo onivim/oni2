be9e2457ec041d9de1663213613f0c2422659b75
bryphe <bryphe@outrunlabs.com>
2019-07-23 12:11:21 -0700

    Update publish scripts

--- scripts/windows/publish.ps1
+++ scripts/windows/publish.ps1
@@ -1,8 +1,12 @@
+$SHORT_COMMIT_ID_UNTRIMMED = (git rev-parse --short HEAD) | Out-String
+$SHORT_COMMIT_ID = $SHORT_COMMIT_ID_UNTRIMMED.Trim()
+
 mkdir -p _publish
 
-Compress-Archive -Path _release/win32 -DestinationPath _publish/Onivim2.zip
+Compress-Archive -Path _release/win32 -DestinationPath _publish/Onivim2-$SHORT_COMMIT_ID.zip
 
 esy create-win-setup
 
 npm install -g innosetup-compiler
 innosetup-compiler _release/setup.iss --O=_publish
+mv _publish/Onivim2-0.2.0-win.exe _publish/Onivim2-$SHORT_COMMIT_ID-x64.exe
