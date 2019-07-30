5f79c56befc742fa1b95e5d53640a386d06edc74
Bryan Phelps <bryphe@outlook.com>
2019-07-30 13:43:54 -0700

    Notarize fixes

--- scripts/osx/notarize.sh
+++ scripts/osx/notarize.sh
@@ -1,24 +1,21 @@
 SHORT_COMMIT_ID=$(git rev-parse --short HEAD)
 
-if [ -z "$OSX_P12_CERTIFICATE" ]
-then
-	echo "No code signing certificate specified."
-else
 	# Script from: https://twocanoes.com/adding-notarization-to-xcode-builds
 	echo "Code signing certificate specified - notarizing DMG"
 
 	uuid=$(uuidgen)
-	echo "Uploading to apple to notarize"
-notarize_uuid=$(xcrun altool --notarize-app --primary-bundle-id "${uuid}" --username $APPLE_DEVELOPER_ID --password $APPLE_DEVELOPER_PASSWORD --file "_publish/Onivim2-$SHORT_COMMIT_ID.dmg" 2>&amp;1 | grep RequestUUID | awk '{print $3'})
+	echo "Uploading to apple to notarize: $uuid"
+notarize_uuid=$(xcrun altool --notarize-app --primary-bundle-id "${uuid}" --username $APPLE_DEVELOPER_ID --password $APPLE_DEVELOPER_PASSWORD --file "_publish/Onivim2-$SHORT_COMMIT_ID.dmg" 2>&1 | grep RequestUUID | awk '{print $3'})
+        echo "Notarize uuid: $notarize_uuid"
 	# Load cert
 
 	success=0
 	for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
 		echo "Checking progress..."
-		progress=$(xcrun altool --notarization-info "${notarize_uuid}" -u $APPLE_DEVELOPER_ID -p $APPLE_DEVELOPER_PASSWORD 2>&amp;1)
+		progress=$(xcrun altool --notarization-info "${notarize_uuid}" -u $APPLE_DEVELOPER_ID -p $APPLE_DEVELOPER_PASSWORD 2>1)
 		echo "${progress}"
 
-		if [$? -ne 0] || [[ "${progress}" =~ "Invalid" ]]; then
+		if [ $? -ne 0 ] || [[ "${progress}" =~ "Invalid" ]]; then
 			echo "Error with notarization. Exiting"
 		fi
 
@@ -35,4 +32,3 @@ notarize_uuid=$(xcrun altool --notarize-app --primary-bundle-id "${uuid}" --user
 		echo "Stapling and running packaging up"
 		xcrun stapler staple "_publish/Onivim2-$SHORT_COMMIT_ID.dmg"
 	fi
-fi
