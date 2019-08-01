caed9d910589396c9fc7136ab69194ab6b5c827e
Bryan Phelps <bryphe@outlook.com>
2019-08-01 15:20:55 -0700

    Distribute tar and dmg

--- scripts/osx/package.sh
+++ scripts/osx/package.sh
@@ -8,5 +8,3 @@ mkdir -p _publish
 appdmg _release/appdmg.json _publish/Onivim2-$SHORT_COMMIT_ID.dmg
 
 tar -C _release -cvzf _publish/Onivim2-$SHORT_COMMIT_ID-darwin.tar.gz Onivim2.App
-
-ditto -c -k --rsrc --keepParent _release/Onivim2.App _publish/Onivim2-$SHORT_COMMIT_ID.App.zip
