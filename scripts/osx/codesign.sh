13ac4941f8f2bf037b7170e4cf839346d8b1c6f2
Bryan Phelps <bryphe@outrunlabs.com>
2019-08-01 19:44:43 -0700

    #510 - Part 5/5 - Fix up signing / notarization scripts (#547)
    * Fix up signing / notarization scripts
    
    * Update codesign.sh
    
    * Package tar as well
    
    * Distribute tar and dmg
    
    * publish -> package

---
+++ scripts/osx/codesign.sh
@@ -0,0 +1,36 @@
+SHORT_COMMIT_ID=$(git rev-parse --short HEAD)
+
+SHORT_COMMIT_ID=$(git rev-parse --short HEAD)	
+
+ if [ -z "$OSX_P12_CERTIFICATE" ]	
+then	
+	echo "No code signing certificate specified."	
+else	
+	echo "Code signing certificate specified"	
+
+ 	# Load cert	
+	echo $OSX_P12_CERTIFICATE | base64 --decode > certificate.p12	
+
+ 	# Create keychain	
+	security create-keychain -p p@ssword1 build.keychain	
+	security default-keychain -s build.keychain	
+	security unlock-keychain -p p@ssword1 build.keychain	
+
+ 	security import certificate.p12 -k build.keychain -P $CODESIGN_PASSWORD -T /usr/bin/codesign	
+
+ 	security set-key-partition-list -S apple-tool:,apple: -s -k p@ssword1 build.keychain	
+
+ 	echo "Checking identities..."	
+
+ 	security find-identity -v
+	echo "Starting codesign..."
+	codesign --deep --force --verbose --sign "Outrun Labs, LLC" _release/Onivim2.App --options runtime --entitlements _release/entitlements.plist
+	echo "Onivim2.App codesign complete!"
+
+	# Validate
+	echo "Validate codesigning..."
+	codesign --verify --deep --strict --verbose=2 _release/Onivim2.App
+	echo "Validation complete!"
+
+        ditto -c -k --rsrc --keepParent _release/Onivim2.App _release/Onivim2.App.zip
+fi
---
+++ scripts/osx/package.sh
@@ -0,0 +1,10 @@
+SHORT_COMMIT_ID=$(git rev-parse --short HEAD)
+
+
+npm install -g appdmg
+
+mkdir -p _publish
+
+appdmg _release/appdmg.json _publish/Onivim2-$SHORT_COMMIT_ID.dmg
+
+tar -C _release -cvzf _publish/Onivim2-$SHORT_COMMIT_ID-darwin.tar.gz Onivim2.App
