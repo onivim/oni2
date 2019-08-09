223827413e84404f4f2899eb6e765ade589c4436
bryphe <bryphe@outrunlabs.com>
2019-08-09 10:32:07 -0700

    Fix release script to delete _release directory if it exists

--- scripts/release.js
+++ scripts/release.js
@@ -10,6 +10,10 @@ console.log("Working directory: " + process.cwd());
 const rootDirectory = process.cwd();
 const releaseDirectory = path.join(process.cwd(), "_release");
 
+// Delete releaseDirectory, and remake
+fs.removeSync(releaseDirectory);
+fs.mkdirpSync(releaseDirectory);
+
 const textmateServiceSourceDirectory = path.join(rootDirectory, "src", "textmate_service");
 const extensionsSourceDirectory = path.join(process.cwd(), "extensions");
 // const extensionsDestDirectory = path.join(platformReleaseDirectory, "extensions");
