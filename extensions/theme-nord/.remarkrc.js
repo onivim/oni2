6ef715a43cfeda88b9c293d2d355a9cc47503a06
Bryan Phelps <bryphe@outrunlabs.com>
2019-10-15 10:30:17 -0500

    Themes: Add 'Nord' (#848)
    * Add nord theme
    
    * bump

---
+++ extensions/theme-nord/.remarkrc.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2017-present Arctic Ice Studio <development@arcticicestudio.com>
+ * Copyright (C) 2017-present Sven Greb <development@svengreb.de>
+ *
+ * Project:    Nord Visual Studio Code
+ * Repository: https://github.com/arcticicestudio/nord-visual-studio-code
+ * License:    MIT
+ */
+
+/**
+ * @file The remark-lint configuration.
+ * @author Arctic Ice Studio <development@arcticicestudio.com>
+ * @author Sven Greb <development@svengreb.de>
+ */
+
+module.exports = {
+  plugins: ["remark-preset-lint-arcticicestudio"]
+};
