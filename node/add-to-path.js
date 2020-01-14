5c2806d321961a69c5094fcf329eb58ed07c498d
CrossR <r.cross@lancaster.ac.uk>
2020-01-14 23:32:59 +0000

    Run prettier.

--- node/add-to-path.js
+++ node/add-to-path.js
@@ -1,10 +1,10 @@
 // add-to-path.js
 // This is a Node-side script to add Oni2 to their path.
 // This is done in node, to get sudo-prompt.
-const fs = require("fs");
-const os = require("os");
-const path = require("path");
-const sudo = require("sudo-prompt");
+const fs = require("fs")
+const os = require("os")
+const path = require("path")
+const sudo = require("sudo-prompt")
 
 const isWindows = () => os.platform() === "win32"
 const isMac = () => os.platform() === "darwin"
@@ -14,7 +14,7 @@ const getLinkPath = () => (isMac() || isLinux() ? "/usr/local/bin/oni2" : "")
 
 const isAddedToPath = () => {
     try {
-        fs.lstatSync(getLinkPath());
+        fs.lstatSync(getLinkPath())
     } catch (_) {
         return false
     }
@@ -24,72 +24,70 @@ const isAddedToPath = () => {
 const getAppDirectory = () => {
     if (isMac()) {
         // Valid for an install from a dmg.
-        return path.join(path.dirname(process.mainModule.filename), "..");
+        return path.join(path.dirname(process.mainModule.filename), "..")
     } else {
         // Valid path for an AppImage or Linux tar.gz
-        return path.join(path.dirname(process.mainModule.filename), "..", "..");
+        return path.join(path.dirname(process.mainModule.filename), "..", "..")
     }
 }
 
 const getOptions = () => {
-
-    let appDirectory = getAppDirectory();
+    let appDirectory = getAppDirectory()
 
     // Valid for an install from a dmg.
-    let imgPath = path.join(appDirectory, "Onivim2.icns");
+    let imgPath = path.join(appDirectory, "Onivim2.icns")
 
     if (!fs.existsSync(imgPath)) {
         // Valid path when in a development build.
-        imgPath = path.join(appDirectory, "assets", "images", "Onivim2.icns");
+        imgPath = path.join(appDirectory, "assets", "images", "Onivim2.icns")
     }
 
     if (!fs.existsSync(imgPath)) {
         // Valid path for an AppImage or Linux tar.gz
-        imgPath = path.join(appDirectory, "..", "Onivim2.icns");
+        imgPath = path.join(appDirectory, "..", "Onivim2.icns")
     }
 
-    const options = { name: "Oni2", icns: imgPath };
+    const options = { name: "Oni2", icns: imgPath }
 
-    return options;
+    return options
 }
 
 const removeFromPath = async () => {
     if (isAddedToPath() && !isWindows()) {
-        await runSudoCommand(`rm ${getLinkPath()}`, getOptions());
+        await runSudoCommand(`rm ${getLinkPath()}`, getOptions())
     }
 }
 
 const addToPath = async () => {
     if (!isAddedToPath() && !isWindows()) {
-
-        let appDirectory = getAppDirectory();
-        let linkDest = "";
+        let appDirectory = getAppDirectory()
+        let linkDest = ""
 
         if (isMac()) {
             // Valid for an install from a dmg.
-            linkDest = path.join(appDirectory, "run.sh");
+            linkDest = path.join(appDirectory, "run.sh")
         } else if (process.env.APPIMAGE) {
             // Valid path for an AppImage.
-            linkDest = process.env.APPIMAGE;
+            linkDest = process.env.APPIMAGE
         } else {
             // Valid path for Linux tar.gz
-            linkDest = path.join(appDirectory, "..", "AppRun");
+            linkDest = path.join(appDirectory, "..", "AppRun")
         }
 
         if (!fs.existsSync(linkDest)) {
             return
         }
 
-        await runSudoCommand(`ln -fs ${linkDest} ${getLinkPath()}`, getOptions());
+        await runSudoCommand(`ln -fs ${linkDest} ${getLinkPath()}`, getOptions())
     }
 }
 
 const runSudoCommand = async (command, options) => {
     return new Promise(resolve => {
         sudo.exec(command, options, (error, stdout, stderr) => {
-            resolve({ error, stdout, stderr });
-        });
-    });
+            resolve({ error, stdout, stderr })
+        })
+    })
 }
 
 const toggleAddToPath = async () => {
@@ -100,8 +98,8 @@ const toggleAddToPath = async () => {
     }
 }
 
-(async () => {
+;(async () => {
     try {
-        await Promise.resolve(toggleAddToPath());
+        await Promise.resolve(toggleAddToPath())
     } catch (_) {}
-})();
+})()
