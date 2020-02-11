ed3740bdb04bded538fe8bc20ffc682efb3657e3
Bryan Phelps <bryphe@outlook.com>
2020-02-10 18:05:36 -0800

    Fix up codesign script to sign node binaries

--- scripts/osx/codesign.sh
+++ scripts/osx/codesign.sh
@@ -1,36 +1,54 @@
 SHORT_COMMIT_ID=$(git rev-parse --short HEAD)
 
-SHORT_COMMIT_ID=$(git rev-parse --short HEAD)	
+manual-codesign() {
+    codesign --force --verbose --sign "Outrun Labs, LLC" _release/Onivim2.app/$1 --options runtime --entitlements _release/entitlements.plist
+}
 
- if [ -z "$OSX_P12_CERTIFICATE" ]	
-then	
-	echo "No code signing certificate specified."	
-else	
-	echo "Code signing certificate specified"	
+if [ -z "$OSX_P12_CERTIFICATE" ]      
+then   
+   echo "No code signing certificate specified."   
+else   
+   echo "Code signing certificate specified"       
 
- 	# Load cert	
-	echo $OSX_P12_CERTIFICATE | base64 --decode > certificate.p12	
+   # Load cert     
+   echo $OSX_P12_CERTIFICATE | base64 --decode > certificate.p12   
 
- 	# Create keychain	
-	security create-keychain -p p@ssword1 build.keychain	
-	security default-keychain -s build.keychain	
-	security unlock-keychain -p p@ssword1 build.keychain	
+   # Create keychain       
+   security create-keychain -p p@ssword1 build.keychain    
+   security default-keychain -s build.keychain     
+   security unlock-keychain -p p@ssword1 build.keychain    
 
- 	security import certificate.p12 -k build.keychain -P $CODESIGN_PASSWORD -T /usr/bin/codesign	
+   security import certificate.p12 -k build.keychain -P $CODESIGN_PASSWORD -T /usr/bin/codesign    
 
- 	security set-key-partition-list -S apple-tool:,apple: -s -k p@ssword1 build.keychain	
+   security set-key-partition-list -S apple-tool:,apple: -s -k p@ssword1 build.keychain    
 
- 	echo "Checking identities..."	
+   echo "Checking identities..."   
 
- 	security find-identity -v
-	echo "Starting codesign..."
-	codesign --deep --force --verbose --sign "Outrun Labs, LLC" _release/Onivim2.app --options runtime --entitlements _release/entitlements.plist
-	echo "Onivim2.app codesign complete!"
+   security find-identity -v
+   echo "Starting codesign..."
 
-	# Validate
-	echo "Validate codesigning..."
-	codesign --verify --deep --strict --verbose=2 _release/Onivim2.app
-	echo "Validation complete!"
+   # Codesign individual files that require it
+   manual-codesign Contents/MacOS/rg
+   manual-codesign Contents/MacOS/rls
+   manual-codesign Contents/MacOS/node
+   manual-codesign Contents/MacOS/Oni2_editor
+   manual-codesign Contents/MacOS/Oni2
+   manual-codesign Contents/Frameworks/libonig.5.dylib
+   manual-codesign Contents/Frameworks/libpng16.16.dylib
+   manual-codesign Contents/Frameworks/libfreetype.6.dylib
+   manual-codesign Contents/Frameworks/libharfbuzz.0.dylib
+   manual-codesign Contents/Frameworks/libSDL2-2.0.0.dylib
+   manual-codesign Contents/Frameworks/libffi.6.dylib
+   manual-codesign Contents/Resources/node/node_modules/node-pty/build/Release/pty.node
 
-        ditto -c -k --rsrc --keepParent _release/Onivim2.app _release/Onivim2.app.zip
-fi
+   # Finish by codesigning the root
+   manual-codesign
+   
+   echo "Onivim2.app codesign complete!"
+
+   # Validate
+   echo "Validate codesigning..."
+   codesign --verify --deep --strict --verbose=2 _release/Onivim2.app
+   echo "Validation complete!"
+
+   ditto -c -k --rsrc --keepParent _release/Onivim2.app _release/Onivim2.app.zip
