55b33e85d4cda7a1fd3dead22f171e50e0b61bb5
Bryan Phelps <bryphe@outlook.com>
2020-04-09 17:02:39 -0700

    Initial tips and tricks

--- docs/website/pages/en/tips-and-tricks.js
+++ docs/website/pages/en/tips-and-tricks.js
@@ -7,48 +7,61 @@
 
 const React = require('react');
 
-const CompLibrary = require('../../core/CompLibrary.js');
 
-const Container = CompLibrary.Container;
-const GridBlock = CompLibrary.GridBlock;
+const tips = [
+{
+  title: "Jump around with G",
+  description: <div>
+  <ul>
+    <li><kbd>gg</kbd> - move to top of buffer</li>
+    <li><kbd>G</kbd> - move to bottom of buffer</li>
+    <li><kbd>50G</kbd> - move to line 50</li>
+  </ul>
+  </div>,
+  videoFile: "demo-gg"
+},
+{
+  title: "Edit Strings like a Ninja",
+  description: <div>
+  <ul>
+    <li><kbd>ci"</kbd> - change inside string</li>
+  </ul>
+  </div>,
+  videoFile: "TODO"
+}
+];
 
-function Help(props) {
-  const {config: siteConfig, language = ''} = props;
-  const {baseUrl, docsUrl} = siteConfig;
-  const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-  const langPart = `${language ? `${language}/` : ''}`;
-  const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
-
-  const supportLinks = [
-    {
-      content: `Learn more using the [documentation on this site.](${docUrl(
-        'doc1.html',
-      )})`,
-      title: 'Browse Docs',
-    },
-    {
-      content: 'Ask questions about the documentation and project',
-      title: 'Join the community',
-    },
-    {
-      content: "Find out what's new with this project",
-      title: 'Stay up to date',
-    },
-  ];
-
-  return (
-    <div className="docMainWrapper wrapper">
-      <Container className="mainContainer documentContainer postContainer">
-        <div className="post">
-          <header className="postHeader">
-            <h1>Need help?</h1>
-          </header>
-          <p>This project is maintained by a dedicated group of people.</p>
-          <GridBlock contents={supportLinks} layout="threeColumn" />
+function Tip(props) {
+  return <section className="tip">
+    <div className="tip-inner">
+      <div className="tip-text">
+        <h2>{props.title}</h2>
+        <div className="description">
+          {props.description}
         </div>
-      </Container>
+      </div>
+      <div className="tip-video">
+        <figure>
+          <video height={300} width={400} autoPlay={true} loop={true} muted={true} playsInline={true}>
+            <source src={"/vid/" + props.videoFile + ".mp4"} type="video/mp4"/>
+            <source src={"/vid/" + props.videoFile + ".webm"} type="video/webm"/>
+          </video>
+        </figure>
+      </div>
     </div>
-  );
+  </section>
 }
 
-module.exports = Help;
+function TipContainer(_props) {
+
+  //const {config: siteConfig, language = ''} = props;
+
+  const result = tips.map((tip) => <Tip title={tip.title}
+  description={tip.description} videoFile={tip.videoFile} />);
+
+  return <div className="tips">
+  {result}
+  </div>
+}
+
+module.exports = TipContainer;
