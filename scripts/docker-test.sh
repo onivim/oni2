47945c1af937b7d3f9409f4ca64d1d22e7642082
Bryan Phelps <bryphe@outlook.com>
2020-04-09 17:09:10 -0700

    Delete 'b' folder to save space

--- scripts/docker-test.sh
+++ scripts/docker-test.sh
@@ -1,6 +1,12 @@
 source /opt/rh/llvm-toolset-7.0/enable
 clang -v
 
+echo "Deleting $(ESY_CACHE_BUILD_PATH)"
+rm -rf $(ESY__CACHE_BUILD_PATH)
+
+echo "Deleting esy folder"
+rm -rf _esy
+
 esy @test install
 esy @test build
 esy @test run
