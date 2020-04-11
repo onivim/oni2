6a6f52a9d3690aa6bd41e3e61c4cf910481bd199
Bryan Phelps <bryphe@outrunlabs.com>
2020-04-11 10:23:58 -0700

    CI - Linux: More disk space issues (#1585)
    * Delete 'b' folder to save space
    
    * Try out esy cleanup command
    
    * Re-order calls
    
    * Try using current package
    
    * Try removing some dependencies
    
    * Remove more
    
    * Fix quotes
    
    * sudo
    
    * Remove extra deletion
    
    * Additional fixes

--- scripts/docker-test.sh
+++ scripts/docker-test.sh
@@ -1,6 +1,12 @@
 source /opt/rh/llvm-toolset-7.0/enable
 clang -v
 
+echo "Deleting esy folder"
+rm -rf _esy
+
+esy cleanup test.json --dry-run
+esy cleanup test.json
+
 esy @test install
 esy @test build
 esy @test run
