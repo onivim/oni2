bb4bb5ecb435056b63513964829ba9ccb76f9413
Bryan Phelps <bryphe@outlook.com>
2020-04-30 15:44:07 -0700

    Remove cleanup prior to tests

--- scripts/docker-test.sh
+++ scripts/docker-test.sh
@@ -1,12 +1,6 @@
 source /opt/rh/llvm-toolset-7.0/enable
 clang -v
 
-echo "Deleting esy folder"
-rm -rf _esy
-
-esy cleanup test.json --dry-run
-esy cleanup test.json
-
 esy @test install
 esy @test build
 esy @test run
