fa6bcb4ffe1b4bd9abc1c942c7715871992af163
Bryan Phelps <bryphe@outlook.com>
2020-07-02 10:39:33 -0700

    Prettier, again

--- scripts/generate-mit-readme.js
+++ scripts/generate-mit-readme.js
@@ -1,18 +1,18 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("fs")
+const path = require("path")
 
-const commitId = process.argv[2];
-const destPath = process.argv[3];
-console.log(`Commit: ${commitId}`);
-console.log(`Destination Folder: ${destPath}`);
+const commitId = process.argv[2]
+const destPath = process.argv[3]
+console.log(`Commit: ${commitId}`)
+console.log(`Destination Folder: ${destPath}`)
 
-const templateMd = path.join(__dirname, "template.md");
-const destinationMd = path.join(destPath, "README.md");
+const templateMd = path.join(__dirname, "template.md")
+const destinationMd = path.join(destPath, "README.md")
 
-const inputLines = fs.readFileSync(templateMd, "utf8");
+const inputLines = fs.readFileSync(templateMd, "utf8")
 
-const lines = inputLines.replace("%%COMMITID%%", commitId);
+const lines = inputLines.replace("%%COMMITID%%", commitId)
 
 console.log(lines)
 
-fs.writeFileSync(destinationMd, lines, "utf8");
+fs.writeFileSync(destinationMd, lines, "utf8")
