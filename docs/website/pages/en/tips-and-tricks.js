c85ab2512cb480c4a5c24a7962acb7715c2fe540
Bryan Phelps <bryphe@outlook.com>
2020-07-09 18:33:23 -0700

    Progress

--- docs/website/pages/en/tips-and-tricks.js
+++ docs/website/pages/en/tips-and-tricks.js
@@ -7,28 +7,52 @@
 
 const React = require('react');
 
+function Key(props) {
+  return <li>
+        <kbd>{props.name}</kbd> {" - " + props.description}</li>;
+};
 
 const tips = [
 {
-  title: "Jump around with G",
-  description: <div>
-  <ul>
-    <li><kbd>gg</kbd> - move to top of buffer</li>
-    <li><kbd>G</kbd> - move to bottom of buffer</li>
-    <li><kbd>50G</kbd> - move to line 50</li>
-  </ul>
-  </div>,
-  videoFile: "demo-gg"
+  title: "Scroll relative to cursor",
+  description: "Use zz, zb, and zt to scroll the buffer, relative to cursor position",
+  keys: [
+    <Key name="zz" description="Center cursor line in viewport" />,
+    <Key name="zt" description="Move viewport such that cursor line is at the top" />,
+    <Key name="zb" description="Move viewport such that cursor line is at the bottom" />,
+  ],
+  videoFile: "RodY2O1xFfI"
 },
 {
-  title: "Edit Strings like a Ninja",
-  description: <div>
-  <ul>
-    <li><kbd>ci"</kbd> - change inside string</li>
-  </ul>
-  </div>,
-  videoFile: "TODO"
+  title: "Jump around the buffer",
+  description: "Use gg and G to quickly move up and down the buffer.",
+  keys: [
+    <Key name="gg" description="move to first line in buffer" />,
+    <Key name="G" description="move to last line in buffer" />,
+    <Key name="50G" description="move to line 50 in buffer" />,
+  ],
+  videoFile: "xh4TTWy-okQ"
 }
+//{
+//  title: "Jump around with G",
+//  description: <div>
+//  <ul>
+//    <li><kbd>gg</kbd> - move to top of buffer</li>
+//    <li><kbd>G</kbd> - move to bottom of buffer</li>
+//    <li><kbd>50G</kbd> - move to line 50</li>
+//  </ul>
+//  </div>,
+//  videoFile: "demo-gg"
+//},
+//{
+//  title: "Edit Strings like a Ninja",
+//  description: <div>
+//  <ul>
+//    <li><kbd>ci"</kbd> - change inside string</li>
+//  </ul>
+//  </div>,
+//  videoFile: "TODO"
+//}
 ];
 
 function Tip(props) {
@@ -39,14 +63,16 @@ function Tip(props) {
         <div className="description">
           {props.description}
         </div>
+        <h3>Keys Used:</h3>
+        <ul children={props.keys} />
       </div>
       <div className="tip-video">
-        <figure>
-          <video height={300} width={400} autoPlay={true} loop={true} muted={true} playsInline={true}>
-            <source src={"/vid/" + props.videoFile + ".mp4"} type="video/mp4"/>
-            <source src={"/vid/" + props.videoFile + ".webm"} type="video/webm"/>
-          </video>
-        </figure>
+        <iframe
+        width="480"
+        height="270"
+        src={"https://www.youtube.com/embed/" + props.videoFile + "?&autoplay=1&controls=0&modestbranding=1&loop=1&playlist=" + props.videoFile}
+        frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
+        </iframe>
       </div>
     </div>
   </section>
@@ -57,7 +83,7 @@ function TipContainer(_props) {
   //const {config: siteConfig, language = ''} = props;
 
   const result = tips.map((tip) => <Tip title={tip.title}
-  description={tip.description} videoFile={tip.videoFile} />);
+  description={tip.description} keys={tip.keys} videoFile={tip.videoFile} />);
 
   return <div className="tips">
   {result}
