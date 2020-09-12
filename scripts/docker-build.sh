9e7a796d9f8de6fea9d700584d2f5230014fd26c
Zach Baylin <zbaylin@outrunlabs.com>
2020-09-12 15:53:28 -0400

    Docker: install @release

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,9 +1,9 @@
 source /opt/rh/llvm-toolset-7.0/enable
 clang -v
 
-esy install
+esy @release install
 esy bootstrap
 node install-node-deps.js --production
-esy build
-esy x Oni2 -f --checkhealth
+esy @release build
+esy @release x Oni2 -f --checkhealth
 esy @release create
