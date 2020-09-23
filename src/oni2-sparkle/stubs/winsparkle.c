cd322a44e26590153e5b65439781b05ea91857ac
Zach Baylin <zbaylin@outrunlabs.com>
2020-09-23 09:31:01 -0500

    Sparkle: separate Sparkle and WinSparkle

---
+++ src/oni2-sparkle/stubs/winsparkle.c
@@ -0,0 +1,110 @@
+#include <windows.h>
+#include <shlwapi.h>
+
+#include "winsparkle.h"
+
+#include "config.h"
+
+#include <caml/alloc.h>
+#include <caml/callback.h>
+#include <caml/memory.h>
+#include <caml/mlvalues.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef USE_WIN_SPARKLE
+
+CAMLprim value oni2_SparkleInit() {
+  CAMLparam0();
+
+  char iniPath[MAX_PATH];
+
+  GetModuleFileName(NULL, iniPath, MAX_PATH);
+  PathRemoveFileSpec(iniPath);
+
+  strcat(iniPath, "\\Oni2.ini");
+  char version[16];
+  wchar_t versionWide[16];
+  
+
+  GetPrivateProfileString("Application", "Version", "", version, 16, iniPath);
+
+  mbstowcs(versionWide, version, 16);
+
+  win_sparkle_set_app_details(L"Outrun Labs LLC", L"Onivim 2", versionWide);
+  win_sparkle_init();
+
+  CAMLreturn(Val_unit);
+}
+
+// On WinSparkle, there is no instance -- the library just uses global variables.
+// So here we just return a basic `unit`
+CAMLprim value oni2_SparkleGetSharedInstance() {
+  CAMLparam0();
+
+  CAMLreturn(Val_unit);
+}
+
+// This function doesn't make too much sense since we lose the OOP aspect on
+// Windows, so it's basically a noop.
+CAMLprim value oni2_SparkleDebugToString(value vData) {
+  CAMLparam1(vData);
+  CAMLlocal1(vStr);
+
+  vStr = caml_copy_string("Unimplemented");
+
+  CAMLreturn(vStr);
+}
+
+// Again, this function doesn't make much sense on Windows, so it's a noop.
+CAMLprim value oni2_SparkleDebugLog(value vData) {
+  CAMLparam1(vData);
+  
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim value oni2_SparkleSetFeedURL(value vUpdater, value vUrlStr) {
+  CAMLparam2(vUpdater, vUrlStr);
+  static int initialized = 0;
+
+
+  win_sparkle_set_appcast_url(String_val(vUrlStr));
+  
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim value oni2_SparkleGetFeedURL(value vUpdater) {
+  CAMLparam1(vUpdater);
+  CAMLlocal1(vStr);
+  
+  vStr = caml_copy_string("Unimplemented");
+
+  CAMLreturn(vStr);
+}
+
+CAMLprim value oni2_SparkleSetAutomaticallyChecksForUpdates(value vUpdater, value vChecks) {
+  CAMLparam2(vUpdater, vChecks);
+
+  win_sparkle_set_automatic_check_for_updates(Bool_val(vChecks));
+
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim value oni2_SparkleGetAutomaticallyChecksForUpdates(value vUpdater) {
+  CAMLparam1(vUpdater);
+
+  int checks = win_sparkle_get_automatic_check_for_updates();
+
+  CAMLreturn(Val_bool(checks));
+}
+
+CAMLprim value oni2_SparkleCheckForUpdates(value vUpdater) {
+  CAMLparam1(vUpdater);
+
+  win_sparkle_check_update_with_ui();
+
+  CAMLreturn(Val_unit);
+}
+
+#endif
