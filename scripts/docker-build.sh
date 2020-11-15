44051fb07abb7102d7d6e96536b07e517e2471bb
Bryan Phelps <bryphe@outrunlabs.com>
2020-11-15 10:51:17 -0800

    Explicitly set concurrency flag

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,15 +1,13 @@
 source /opt/rh/llvm-toolset-7.0/enable
 clang -v
 
-ESY__BUILD_CONCURRENCY=1
-ESY__FETCH_CONCURRENCY=1
-esy install -vv
-esy build -vv
-esy bootstrap -vv
+esy install --fetch-concurrency=1 --build-concurrency=1 -vv
+esy build --fetch-concurrency=1 --build-concurrency=1 -vv
+esy bootstrap --fetch-concurrency=1 --build-concurrency=1 -vv
 node install-node-deps.js --production
 esy x Oni2 -f --checkhealth
 
-esy @release install
-esy @release build
-esy @release x Oni2 -f --checkhealth
-esy @release create
+# esy @release install
+# esy @release build
+# esy @release x Oni2 -f --checkhealth
+# esy @release create
