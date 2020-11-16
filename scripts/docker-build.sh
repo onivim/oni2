9547cb3ea5c24205065c929d5408b5e143641383
Bryan Phelps <bryphe@outrunlabs.com>
2020-11-16 07:14:45 -0800

    Consolidate build/test run

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -7,6 +7,11 @@ esy bootstrap --fetch-concurrency=1 --build-concurrency=1 -vv
 node install-node-deps.js --production
 esy x Oni2 -f --checkhealth
 
+esy @test install --fetch-concurrency=1 --build-concurrency=1
+esy @test build --fetch-concurrency=1 --build-concurrency=1
+esy @test run
+esy @test inline
+
 esy @release install --fetch-concurrency=1 --build-concurrency=1
 esy @release build --fetch-concurrency=1 --build-concurrency=1
 esy @release x Oni2 -f --checkhealth
