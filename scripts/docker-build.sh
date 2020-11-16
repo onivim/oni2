9439c2b9ecbfa99fb7db64c4e996aa0dbff6b0e7
Bryan Phelps <bryphe@outrunlabs.com>
2020-11-16 08:36:25 -0800

    chore(ci): Fix CentOS / docker esy install failure (#2715)
    Fix hang on `esy install` in CentOS docker image

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,13 +1,18 @@
 source /opt/rh/llvm-toolset-7.0/enable
 clang -v
 
-esy install
-esy build
-esy bootstrap
+esy install --fetch-concurrency=1 --build-concurrency=1 -vv
+esy build --fetch-concurrency=1 --build-concurrency=1 -vv
+esy bootstrap --fetch-concurrency=1 --build-concurrency=1 -vv
 node install-node-deps.js --production
 esy x Oni2 -f --checkhealth
 
-esy @release install
-esy @release build
+esy @test install --fetch-concurrency=1 --build-concurrency=1
+esy @test build --fetch-concurrency=1 --build-concurrency=1
+esy @test run
+esy @test inline
+
+esy @release install --fetch-concurrency=1 --build-concurrency=1
+esy @release build --fetch-concurrency=1 --build-concurrency=1
 esy @release x Oni2 -f --checkhealth
 esy @release create
