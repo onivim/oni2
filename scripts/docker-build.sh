a1f723ab660044eca561bc5e982ddd0b2bda053a
Bryan Phelps <bryphe@outrunlabs.com>
2020-11-17 14:57:03 -0800

    Switch to env variables

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,19 +1,26 @@
 source /opt/rh/llvm-toolset-7.0/enable
 clang -v
 
+# Use build cache to speed up PR builds
 export ESY__PREFIX=/esy/store
-esy install --fetch-concurrency=1 --build-concurrency=1
-esy build --fetch-concurrency=1 --build-concurrency=1
+
+# Workaround for: https://github.com/esy/esy/issues/1227
+# Concurrent fetch seems to cause hang on Docker in Azure Pipelines..
+export ESY__BUILD_CONCURRENCY=1
+export ESY__FETCH_CONCURRENCY=1
+
+esy install 
+esy build 
 esy run-script bootstrap
 node install-node-deps.js --production
 esy x  Oni2 -f --checkhealth
 
-esy @test install --fetch-concurrency=1 --build-concurrency=1
-esy @test build --fetch-concurrency=1 --build-concurrency=1
+esy @test install 
+esy @test build 
 esy @test run-script run
 esy @test run-script inline
 
-esy @release install --fetch-concurrency=1 --build-concurrency=1 
-esy @release build --fetch-concurrency=1 --build-concurrency=1 
+esy @release install 
+esy @release build 
 esy @release x Oni2 -f --checkhealth
 esy @release run-script create
