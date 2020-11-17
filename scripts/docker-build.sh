34b3a956c70816da2b1ab11929d2995462831cb4
Bryan Phelps <bryphe@outrunlabs.com>
2020-11-17 11:47:37 -0800

    Use run-script to pass prefix-path

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,16 +3,16 @@ clang -v
 
 esy install --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
 esy build --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
-esy bootstrap --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
+esy run-script --prefix-path=/esy/store bootstrap
 node install-node-deps.js --production
-esy x Oni2 -f --checkhealth
+esy x --prefix-path=/esy/store Oni2 -f --checkhealth
 
 esy @test install --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
 esy @test build --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
-esy @test run --prefix-path=/esy/store
-esy @test inline --prefix-path=/esy/store
+esy @test run-script --prefix-path=/esy/store run
+esy @test run-script --prefix-path=/esy/store inline
 
 esy @release install --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
 esy @release build --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
-esy @release x Oni2 -f --checkhealth
-esy @release create
+esy @release x --prefix-path=/esy/store Oni2 -f --checkhealth
+esy @release run-script --prefix-path=/esy/store create
