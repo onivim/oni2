85332ef852ff6a114686eae5a1113932e06a44e3
Bryan Phelps <bryphe@outrunlabs.com>
2020-11-17 12:46:18 -0800

    Try env instead

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,18 +1,19 @@
 source /opt/rh/llvm-toolset-7.0/enable
 clang -v
 
-esy install --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
-esy build --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
-esy run-script --prefix-path=/esy/store bootstrap
+export ESY_PREFIX=/esy/store
+esy install --fetch-concurrency=1 --build-concurrency=1
+esy build --fetch-concurrency=1 --build-concurrency=1
+esy run-script bootstrap
 node install-node-deps.js --production
-esy x --prefix-path=/esy/store Oni2 -f --checkhealth
+esy x  Oni2 -f --checkhealth
 
-esy @test install --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
-esy @test build --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
-esy @test run-script --prefix-path=/esy/store run
-esy @test run-script --prefix-path=/esy/store inline
+esy @test install --fetch-concurrency=1 --build-concurrency=1
+esy @test build --fetch-concurrency=1 --build-concurrency=1
+esy @test run-script run
+esy @test run-script inline
 
-esy @release install --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
-esy @release build --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
-esy @release x --prefix-path=/esy/store Oni2 -f --checkhealth
-esy @release run-script --prefix-path=/esy/store create
+esy @release install --fetch-concurrency=1 --build-concurrency=1 
+esy @release build --fetch-concurrency=1 --build-concurrency=1 
+esy @release x Oni2 -f --checkhealth
+esy @release run-script create
