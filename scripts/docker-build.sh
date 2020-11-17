82d6871dbc3051bfeae783deb656ab1f881218ee
Bryan Phelps <bryphe@outrunlabs.com>
2020-11-16 18:00:08 -0800

    chore(ci): CentOS - Docker - Mount esy directory for caching (#2724)

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,18 +1,18 @@
 source /opt/rh/llvm-toolset-7.0/enable
 clang -v
 
-esy install --fetch-concurrency=1 --build-concurrency=1 -vv
-esy build --fetch-concurrency=1 --build-concurrency=1 -vv
-esy bootstrap --fetch-concurrency=1 --build-concurrency=1 -vv
+esy install --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
+esy build --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
+esy bootstrap --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
 node install-node-deps.js --production
 esy x Oni2 -f --checkhealth
 
-esy @test install --fetch-concurrency=1 --build-concurrency=1
-esy @test build --fetch-concurrency=1 --build-concurrency=1
-esy @test run
-esy @test inline
+esy @test install --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
+esy @test build --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
+esy @test run --prefix-path=/esy/store
+esy @test inline --prefix-path=/esy/store
 
-esy @release install --fetch-concurrency=1 --build-concurrency=1
-esy @release build --fetch-concurrency=1 --build-concurrency=1
+esy @release install --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
+esy @release build --fetch-concurrency=1 --build-concurrency=1 --prefix-path=/esy/store
 esy @release x Oni2 -f --checkhealth
 esy @release create
