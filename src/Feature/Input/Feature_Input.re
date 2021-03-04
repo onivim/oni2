onivim/oni2

commit c54a9e903337ebb4020c7e952e2b89d0b62c09d9

refactor(config): Migrate workbench values from legacy configuration (#3188)

src/Store/Features.re
@@ -744,15 +744,16 @@ let update =
     (state, eff);
 
   | Pane(msg) =>
+    let config = Selectors.configResolver(state);
     let (model, outmsg) =
       Feature_Pane.update(
         ~font=state.editorFont,
         ~languageInfo=state.languageInfo,
         ~buffers=state.buffers,
         ~previewEnabled=
-          Oni_Core.Configuration.getValue(
-            c => c.workbenchEditorEnablePreview,
-            state.configuration,
+          Feature_Configuration.GlobalConfiguration.Workbench.editorEnablePreview.
+            get(
+            config,
           ),
         msg,
         state.pane,
@@ -847,12 +848,13 @@ let update =
     ({...state, registration: state'}, effect);
 
   | Search(msg) =>
+    let config = Selectors.configResolver(state);
     let (model, maybeOutmsg) =
       Feature_Search.update(
         ~previewEnabled=
-          Oni_Core.Configuration.getValue(
-            c => c.workbenchEditorEnablePreview,
-            state.configuration,
+          Feature_Configuration.GlobalConfiguration.Workbench.editorEnablePreview.
+            get(
+            config,
           ),
         state.searchPane,
         msg,
@@ -882,12 +884,13 @@ let update =
     };
 
   | SCM(msg) =>
+    let config = Selectors.configResolver(state);
     let (model, maybeOutmsg) =
       Feature_SCM.update(
         ~previewEnabled=
-          Oni_Core.Configuration.getValue(
-            c => c.workbenchEditorEnablePreview,
-            state.configuration,
+          Feature_Configuration.GlobalConfiguration.Workbench.editorEnablePreview.
+            get(
+            config,
           ),
         ~fileSystem=state.fileSystem,
         extHostClient,

src/Components/FileExplorer/Component_FileExplorer.re
@@ -277,9 +277,9 @@ let update = (~config, ~configuration, msg, model) => {
       // Set active here to avoid scrolling in BufferEnter
       (
         model |> setActive(Some(node.path)),
-        Oni_Core.Configuration.getValue(
-          c => c.workbenchEditorEnablePreview,
-          configuration,
+        Feature_Configuration.GlobalConfiguration.Workbench.editorEnablePreview.
+          get(
+          config,
         )
           ? PreviewFile(FpExp.toString(node.path))
           : OpenFile(FpExp.toString(node.path)),

src/Core/ConfigurationParser.re
@@ -63,24 +63,6 @@ let configurationParsers: list(configurationTuple) = [
     (config, json) => {...config, filesExclude: parseStringList(json)},
   ),
   (
-    "workbench.activityBar.visible",
-    (config, json) => {
-      ...config,
-      workbenchActivityBarVisible: parseBool(json),
-    },
-  ),
-  (
-    "workbench.editor.showTabs",
-    (config, json) => {...config, workbenchEditorShowTabs: parseBool(json)},
-  ),
-  (
-    "workbench.editor.enablePreview",
-    (config, json) => {
-      ...config,
-      workbenchEditorEnablePreview: parseBool(json),
-    },
-  ),
-  (
     "editor.zenMode.hideTabs",
     (config, json) => {...config, zenModeHideTabs: parseBool(json)},
   ),

src/Core/ConfigurationValues.re
@@ -12,9 +12,6 @@ type vimUseSystemClipboard = {
 };
 
 type t = {
-  workbenchActivityBarVisible: bool,
-  workbenchEditorShowTabs: bool,
-  workbenchEditorEnablePreview: bool,
   filesExclude: list(string),
   vsync: Revery.Vsync.t,
   vimUseSystemClipboard,
@@ -28,9 +25,6 @@ type t = {
 };
 
 let default = {
-  workbenchActivityBarVisible: true,
-  workbenchEditorShowTabs: true,
-  workbenchEditorEnablePreview: true,
   filesExclude: ["_esy", "node_modules", ".git"],
   vimUseSystemClipboard: {
     yank: true,

src/Feature/Configuration/GlobalConfiguration.re
@@ -225,6 +225,17 @@ module Explorer = {
     );
 };
 
+module Workbench = {
+  let activityBarVisible =
+    setting("workbench.activityBar.visible", bool, ~default=true);
+
+  let editorShowTabs =
+    setting("workbench.editor.showTabs", bool, ~default=true);
+
+  let editorEnablePreview =
+    setting("workbench.editor.enablePreview", bool, ~default=true);
+};
+
 let contributions = [
   inactiveWindowOpacity.spec,
   animation.spec,
@@ -233,4 +244,7 @@ let contributions = [
   Editor.largeFileOptimizations.spec,
   Editor.snippetSuggestions.spec,
   Explorer.autoReveal.spec,
+  Workbench.activityBarVisible.spec,
+  Workbench.editorShowTabs.spec,
+  Workbench.editorEnablePreview.spec,
 ];

src/UI/EditorView.re
@@ -281,9 +281,12 @@ let make =
       <Parts.EditorContainer editor state theme isActive dispatch />;
   };
 
+  let config = Selectors.configResolver(state);
+
   let editorShowTabs =
-    state.configuration
-    |> Oni_Core.Configuration.getValue(c => c.workbenchEditorShowTabs);
+    Feature_Configuration.GlobalConfiguration.Workbench.editorShowTabs.get(
+      config,
+    );
 
   let hideZenModeTabs =
     state.configuration
@@ -301,7 +304,7 @@ let make =
       isZenMode={state.zenMode}
       showTabs
       model={state.layout}
-      config={Selectors.configResolver(state)}
+      config
       dispatch={msg => dispatch(Actions.Layout(msg))}>
       ...(module ContentProvider)
     </Feature_Layout.View>

src/UI/Root.re
@@ -112,8 +112,9 @@ let make = (~dispatch, ~state: State.t, ()) => {
     };
 
   let activityBar = () =>
-    if (Selectors.getActiveConfigurationValue(state, c =>
-          c.workbenchActivityBarVisible
+    if (Feature_Configuration.GlobalConfiguration.Workbench.activityBarVisible.
+          get(
+          config,
         )
         && !zenMode) {
       <Dock font={state.uiFont} theme sideBar extensions={state.extensions} />;

commit 55089d19a366754c34794bcce8a9caa3515cd89e

fix(keybindings): Fix 'Open keybindings file' command (#3226)
While investigating #3206 - I observed that the 'Preferences: Open keybindings file' was failing to actually open the file.

This looks like a regression from https://github.com/onivim/oni2/pull/3188 , which inactivated the `OpenConfigFile` action. 

This fixes it by plumbing the command back in, via the `Feature_Input` command list.

> WORKAROUND: `oni2 ~/.config/oni2/keybindings.json` on OSX / Linux

src/Feature/Input/Feature_Input.re
@@ -177,6 +177,7 @@ type outmsg =
       toKeys: string,
       error: string,
     })
+  | OpenFile(FpExp.t(FpExp.absolute))
   | TimedOut;
 
 type execute =
@@ -191,7 +192,8 @@ type execute =
 type command =
   | ShowDebugInput
   | EnableKeyDisplayer
-  | DisableKeyDisplayer;
+  | DisableKeyDisplayer
+  | OpenKeybindingsFile;
 
 [@deriving show]
 type msg =
@@ -543,6 +545,13 @@ module Internal = {
 
 let update = (msg, model) => {
   switch (msg) {
+  | Command(OpenKeybindingsFile) =>
+    let eff =
+      model.keybindingLoader
+      |> KeybindingsLoader.getFilePath
+      |> Option.map(path => OpenFile(path))
+      |> Option.value(~default=Nothing);
+    (model, eff);
   | Command(ShowDebugInput) => (model, DebugInputShown)
   | Command(DisableKeyDisplayer) => (
       {...model, keyDisplayer: None},
@@ -644,6 +653,14 @@ let update = (msg, model) => {
 module Commands = {
   open Feature_Commands.Schema;
 
+  let openDefaultKeybindingsFile =
+    define(
+      ~category="Preferences",
+      ~title="Open keybindings file",
+      "workbench.action.openDefaultKeybindingsFile",
+      Command(OpenKeybindingsFile),
+    );
+
   let showInputState =
     define(
       ~category="Debug",
@@ -719,7 +736,12 @@ module ContextKeys = {
 
 module Contributions = {
   let commands =
-    Commands.[showInputState, enableKeyDisplayer, disableKeyDisplayer];
+    Commands.[
+      showInputState,
+      enableKeyDisplayer,
+      disableKeyDisplayer,
+      openDefaultKeybindingsFile,
+    ];
 
   let configuration = Configuration.[leaderKey.spec, timeout.spec];
 

src/Feature/Input/Feature_Input.rei
@@ -13,6 +13,7 @@ type outmsg =
       toKeys: string,
       error: string,
     })
+  | OpenFile(FpExp.t(FpExp.absolute))
   | TimedOut;
 
 [@deriving show]

src/Feature/Input/KeybindingsLoader.re
@@ -47,6 +47,11 @@ let notifyFileSaved = path =>
       orig;
     };
 
+let getFilePath: t => option(FpExp.t(FpExp.absolute)) =
+  fun
+  | None => None
+  | File({filePath, _}) => Some(filePath);
+
 let sub =
   fun
   | None => Isolinear.Sub.none

src/Model/Actions.re
@@ -109,7 +109,6 @@ type t =
       option([ | `Horizontal | `Vertical | `NewTab]),
       option(CharacterPosition.t),
     )
-  | OpenConfigFile(string)
   | Pasted({
       rawText: string,
       isMultiLine: bool,

src/Model/GlobalCommands.re
@@ -21,6 +21,8 @@ let copyFilePath =
     CopyActiveFilepathToClipboard,
   );
 
+let noop = register("noop", Noop);
+
 let command = cmd => CommandInvoked({command: cmd, arguments: `Null});
 
 let undo = register("undo", command("undo"));
@@ -97,14 +99,6 @@ module Oni = {
 
 module Workbench = {
   module Action = {
-    let openDefaultKeybindingsFile =
-      register(
-        ~category="Preferences",
-        ~title="Open keybindings file",
-        "workbench.action.openDefaultKeybindingsFile",
-        OpenConfigFile("keybindings.json"),
-      );
-
     let showCommands =
       register(
         ~title="Show All Commands",

src/Store/Features.re
@@ -560,6 +560,9 @@ let update =
             error,
           ),
         )
+
+      | OpenFile(fp) => Internal.openFileEffect(FpExp.toString(fp))
+
       | TimedOut =>
         Isolinear.Effect.createWithDispatch(~name="Input.timeout", dispatch =>
           dispatch(KeyTimeout)