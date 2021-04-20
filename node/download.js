ddb4c3c4e71cdfd1f41f8c3790fdf5e807861ae3
Bryan Phelps <bryphe@outrunlabs.com>
2021-04-20 15:39:21 -0700

    Format download/request scripts

--- node/download.js
+++ node/download.js
@@ -1,19 +1,21 @@
-const requestLight = require("@onivim/request-light");
+const requestLight = require("@onivim/request-light")
 
 const fs = require("fs")
 
-const url = process.argv[2];
+const url = process.argv[2]
 const destPath = process.argv[3]
 const file = fs.createWriteStream(destPath)
 
-requestLight.xhr({
-	type: "get",
-	url,
-	writeStream: file,
-}).then((response => {
-	if (response.status == 200) {
-		process.stdout.write(`Download of ${url} was successful.`);
-	} else {
-		process.stdout.write(`Download of ${url} failed - status code: {response.status}`);
-	}
-}));
+requestLight
+    .xhr({
+        type: "get",
+        url,
+        writeStream: file,
+    })
+    .then((response) => {
+        if (response.status == 200) {
+            process.stdout.write(`Download of ${url} was successful.`)
+        } else {
+            process.stdout.write(`Download of ${url} failed - status code: {response.status}`)
+        }
+    })
--- node/request.js
+++ node/request.js
@@ -1,11 +1,13 @@
-const requestLight = require("@onivim/request-light");
+const requestLight = require("@onivim/request-light")
 const url = process.argv[2]
 
-requestLight.xhr({
-	type: "get",
-	url: url
-}).then((response) => {
-	if (response.status == 200) {
-		process.stdout.write(response.responseText)
-	}
-});
+requestLight
+    .xhr({
+        type: "get",
+        url: url,
+    })
+    .then((response) => {
+        if (response.status == 200) {
+            process.stdout.write(response.responseText)
+        }
+    })
