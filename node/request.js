22356e4629de3590c923f8281dc6cac7bbd2858d
Bryan Phelps <bryphe@outrunlabs.com>
2021-04-19 17:09:09 -0700

    Use modified request-light package

--- node/request.js
+++ node/request.js
@@ -1,11 +1,11 @@
-const requestLight = require("request-light");
+const requestLight = require("@onivim/request-light");
 const url = process.argv[2]
 
 requestLight.xhr({
-    type: "get",
-    url: url
+	type: "get",
+	url: url
 }).then((response) => {
-    if (response.status == 200) {
-        process.stdout.write(response.responseText)
-    }
+	if (response.status == 200) {
+		process.stdout.write(response.responseText)
+	}
 });
