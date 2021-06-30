onivim/oni2

commit 8e2bb1e8364a2fab3c2261560cc49b3b3cec440d

fix(terminal): Auto-scroll when key is pressed (#3645)
__Issue:__ If the terminal is scrolled, and the user starts typing a command, the terminal doesn't automatically scroll

__Fix:__ Implement auto-scroll when the terminal is fully scrolled.

This also lays some of the ground work for mouse-selection (#2342) - notably, tracking the `scrollY` in the terminal model, because this is needed for auto-scroll when selecting near the boundaries of the terminal.

src/Feature/Terminal/TerminalView.re
@@ -28,17 +28,10 @@ module Terminal = {
                   ~isActive,
                   ~config,
                   ~terminal: Model.terminal,
-                  ~font: Service_Font.font,
                   ~theme: Oni_Core.ColorTheme.Colors.t,
                   ~dispatch,
                   (),
                 ) => {
-    let resolvedFont =
-      Service_Font.resolveWithFallback(
-        Revery.Font.Weight.Normal,
-        font.fontFamily,
-      );
-
     let opacity =
       isActive
         ? 1.0
@@ -46,13 +39,7 @@ module Terminal = {
             config,
           );
 
-    let lineHeight =
-      Configuration.lineHeight.get(config)
-      |> Oni_Core.Utility.OptionEx.value_or_lazy(() => {
-           Feature_Configuration.GlobalConfiguration.Editor.lineHeight.get(
-             config,
-           )
-         });
+    let font = terminal.font;
 
     let%hook lastDimensions = Hooks.ref(None);
 
@@ -65,7 +52,7 @@ module Terminal = {
         If((!=), terminal.id),
         () => {
           lastDimensions^
-          |> Option.iter(((rows, columns)) => {
+          |> Option.iter(((rows, columns, _pixelHeight)) => {
                dispatch(Model.Resized({id: terminal.id, rows, columns}))
              });
 
@@ -73,17 +60,8 @@ module Terminal = {
         },
       );
 
-    let Service_Font.{fontSize, smoothing, _} = font;
+    let terminalFont = font;
 
-    let lineHeightSize =
-      Oni_Core.LineHeight.calculate(~measuredFontHeight=fontSize, lineHeight);
-    let terminalFont =
-      EditorTerminal.Font.make(
-        ~smoothing,
-        ~size=fontSize,
-        ~lineHeight=lineHeightSize,
-        resolvedFont,
-      );
     let onDimensionsChanged =
         (
           {height, width, _}: Revery.UI.NodeEvents.DimensionsChangedEventParams.t,
@@ -94,7 +72,7 @@ module Terminal = {
       let columns =
         float_of_int(width) /. terminalFont.characterWidth |> int_of_float;
 
-      lastDimensions := Some((rows, columns));
+      lastDimensions := Some((rows, columns, height));
       dispatch(Model.Resized({id: terminal.id, rows, columns}));
     };
 
@@ -111,14 +89,47 @@ module Terminal = {
         ~theme=terminalTheme,
         ~cursor,
         ~font=terminalFont,
-        ~scrollBarThickness=Constants.scrollBarThickness,
-        ~scrollBarThumb=Constants.scrollThumbColor,
-        ~scrollBarBackground=Constants.scrollTrackColor,
+        ~scrollY=terminal.scrollY,
         screen,
       );
     };
-    <View onDimensionsChanged style={Styles.container(opacity)}>
-      element
+
+    let onMouseWheel = ({deltaY, _}: NodeEvents.mouseWheelEventParams) => {
+      dispatch(
+        Terminal({
+          id: terminal.id,
+          msg: MouseWheelScrolled({deltaY: deltaY *. 25.0}),
+        }),
+      );
+    };
+
+    let pixelHeight =
+      switch (lastDimensions^) {
+      | None => Terminal.viewportHeight(terminal)
+      | Some((_, _, pixelY)) => float(pixelY)
+      };
+
+    let scrollBar =
+      <Oni_Components.Scrollbar
+        config
+        theme
+        pixelHeight
+        viewportHeight={Terminal.viewportHeight(terminal)}
+        totalHeight={Terminal.totalHeight(terminal)}
+        scrollY={terminal.scrollY}
+        onScroll={scrollY =>
+          dispatch(
+            Terminal({
+              id: terminal.id,
+              msg: ScrollbarMoved({scrollY: scrollY}),
+            }),
+          )
+        }
+      />;
+
+    let elems = [element, scrollBar] |> React.listToElement;
+    <View onDimensionsChanged onMouseWheel style={Styles.container(opacity)}>
+      elems
     </View>;
   };
 };
@@ -129,7 +140,6 @@ let make =
       ~config,
       ~id: int,
       ~terminals: Model.t,
-      ~font: Service_Font.font,
       ~theme: Oni_Core.ColorTheme.Colors.t,
       ~dispatch,
       (),
@@ -137,7 +147,7 @@ let make =
   terminals
   |> Model.getTerminalOpt(id)
   |> Option.map(terminal => {
-       <Terminal config isActive theme font terminal dispatch />
+       <Terminal config isActive theme terminal dispatch />
      })
   |> Option.value(~default=Revery.UI.React.empty);
 };

src/editor-terminal/TerminalView.re
@@ -32,34 +32,21 @@ type terminalSize = {
   height: int,
 };
 
-let%component make =
-              (
-                ~defaultBackground=?,
-                ~defaultForeground=?,
-                ~opacity=1.0,
-                ~scrollBarBackground=Colors.black,
-                ~scrollBarThumb=Colors.darkGray,
-                ~scrollBarThickness=8,
-                ~theme=Theme.default,
-                ~screen: Screen.t,
-                ~cursor: Cursor.t,
-                ~font: Font.t,
-                (),
-              ) => {
-  let%hook (size, setSize) = Hooks.state({width: 0, height: 0});
-  let%hook (userScrollY, setUserScrollY) = Hooks.state(None);
-
+let make =
+    (
+      ~defaultBackground=?,
+      ~defaultForeground=?,
+      ~opacity=1.0,
+      ~theme=Theme.default,
+      ~scrollY: float,
+      ~screen: Screen.t,
+      ~cursor: Cursor.t,
+      ~font: Font.t,
+      (),
+    ) => {
   let totalRows = Screen.getTotalRows(screen);
   let screenRows = Screen.getVisibleRows(screen);
   let scrollBackRows = totalRows - screenRows;
-
-  let screenScrollY = float(scrollBackRows) *. font.lineHeight;
-
-  let scrollY =
-    switch (userScrollY) {
-    | Some(v) => v
-    | None => screenScrollY
-    };
 
   let bg =
     switch (defaultBackground) {
@@ -85,189 +72,152 @@ let%component make =
     )
     |> Revery.Color.toSkia;
 
-  let onScroll = y => {
-    let y = y <= 0. ? 0. : y;
-    let maxScroll = float(scrollBackRows) *. font.lineHeight;
-    let y = y >= maxScroll ? maxScroll : y;
-
-    if (Float.abs(y -. screenScrollY) <= 10.0) {
-      setUserScrollY(_ => None);
-    } else {
-      setUserScrollY(_ => Some(y));
-    };
-  };
-
-  let onWheel = ({deltaY, _}: NodeEvents.mouseWheelEventParams) => {
-    let newScroll = scrollY -. deltaY *. 25.0;
-    onScroll(newScroll);
-  };
-
   let element =
-    <View
+    <Canvas
       style={Styles.container(bg)}
-      onMouseWheel=onWheel
-      onDimensionsChanged={({width, height, _}) => {
-        setSize(_ => {width, height})
-      }}>
-      <Canvas
-        style={Styles.container(bg)}
-        render={(canvasContext, _) => {
-          let {
-            font,
-            lineHeight,
-            characterWidth,
-            characterHeight,
-            fontSize,
-            smoothing,
-          }: Font.t = font;
-          let defaultBackgroundColor = bg |> Color.toSkia;
+      render={(canvasContext, _) => {
+        let {
+          font,
+          lineHeight,
+          characterWidth,
+          characterHeight,
+          fontSize,
+          smoothing,
+        }: Font.t = font;
+        let defaultBackgroundColor = bg |> Color.toSkia;
 
-          let backgroundPaint = Skia.Paint.make();
-          Skia.Paint.setAntiAlias(backgroundPaint, false);
+        let backgroundPaint = Skia.Paint.make();
+        Skia.Paint.setAntiAlias(backgroundPaint, false);
 
-          let textPaint = Skia.Paint.make();
-          let typeFace = Revery.Font.getSkiaTypeface(font);
-          Skia.Paint.setTypeface(textPaint, typeFace);
-          Skia.Paint.setTextSize(textPaint, fontSize);
-          Revery.Font.Smoothing.setPaint(~smoothing, textPaint);
+        let textPaint = Skia.Paint.make();
+        let typeFace = Revery.Font.getSkiaTypeface(font);
+        Skia.Paint.setTypeface(textPaint, typeFace);
+        Skia.Paint.setTextSize(textPaint, fontSize);
+        Revery.Font.Smoothing.setPaint(~smoothing, textPaint);
 
-          Skia.Paint.setLcdRenderText(textPaint, true);
+        Skia.Paint.setLcdRenderText(textPaint, true);
 
-          let lineSpacingOffset =
-            max(0., (lineHeight -. characterHeight) /. 2.);
+        let lineSpacingOffset =
+          max(0., (lineHeight -. characterHeight) /. 2.);
 
-          let columns = Screen.getColumns(screen);
-          let rows = Screen.getTotalRows(screen);
+        let columns = Screen.getColumns(screen);
+        let rows = Screen.getTotalRows(screen);
 
-          let renderBackground = (row, yOffset) =>
-            {let accumulator =
-               ref(
-                 BackgroundColorAccumulator.create(
-                   (startColumn, endColumn, color) =>
-                   if (color !== defaultBackgroundColor) {
-                     Skia.Paint.setColor(backgroundPaint, color);
-                     Skia.Paint.setAlpha(backgroundPaint, opacity);
-                     CanvasContext.drawRectLtwh(
-                       ~paint=backgroundPaint,
-                       ~left=float(startColumn) *. characterWidth,
-                       ~top=yOffset +. lineSpacingOffset,
-                       ~height=lineHeight,
-                       ~width=
-                         float(endColumn - startColumn) *. characterWidth,
+        let renderBackground = (row, yOffset) =>
+          {let accumulator =
+             ref(
+               BackgroundColorAccumulator.create(
+                 (startColumn, endColumn, color) =>
+                 if (color !== defaultBackgroundColor) {
+                   Skia.Paint.setColor(backgroundPaint, color);
+                   Skia.Paint.setAlpha(backgroundPaint, opacity);
+                   CanvasContext.drawRectLtwh(
+                     ~paint=backgroundPaint,
+                     ~left=float(startColumn) *. characterWidth,
+                     ~top=yOffset +. lineSpacingOffset,
+                     ~height=lineHeight,
+                     ~width=float(endColumn - startColumn) *. characterWidth,
+                     canvasContext,
+                   );
+                 }
+               ),
+             );
+           for (column in 0 to columns - 1) {
+             let cell = Screen.getCell(~row, ~column, screen);
+
+             let bgColor = getBgColor(cell);
+             let item = BackgroundColorAccumulator.{column, color: bgColor};
+             accumulator := Accumulator.add(item, accumulator^);
+           };
+           Accumulator.flush(accumulator^)};
+
+        let renderText = (row, yOffset) =>
+          {Skia.Paint.setTextEncoding(textPaint, GlyphId);
+           let accumulator =
+             ref(
+               TextAccumulator.create((startColumn, buffer, color) => {
+                 Skia.Paint.setColor(textPaint, color);
+                 Skia.Paint.setAlpha(textPaint, opacity);
+                 let str = Buffer.contents(buffer);
+                 let tokens =
+                   str
+                   |> Revery.Font.shape(font)
+                   |> Revery.Font.ShapeResult.getGlyphStrings;
+                 List.iter(
+                   ((skiaFace, str)) => {
+                     Skia.Paint.setTypeface(textPaint, skiaFace);
+
+                     CanvasContext.drawText(
+                       ~paint=textPaint,
+                       ~x=float(startColumn) *. characterWidth,
+                       ~y=yOffset +. characterHeight +. lineSpacingOffset,
+                       ~text=str,
                        canvasContext,
                      );
-                   }
-                 ),
-               );
-             for (column in 0 to columns - 1) {
-               let cell = Screen.getCell(~row, ~column, screen);
+                   },
+                   tokens,
+                 );
+               }),
+             );
 
-               let bgColor = getBgColor(cell);
-               let item = BackgroundColorAccumulator.{column, color: bgColor};
-               accumulator := Accumulator.add(item, accumulator^);
-             };
-             Accumulator.flush(accumulator^)};
+           for (column in 0 to columns - 1) {
+             let cell = Screen.getCell(~row, ~column, screen);
 
-          let renderText = (row, yOffset) =>
-            {Skia.Paint.setTextEncoding(textPaint, GlyphId);
-             let accumulator =
-               ref(
-                 TextAccumulator.create((startColumn, buffer, color) => {
-                   Skia.Paint.setColor(textPaint, color);
-                   Skia.Paint.setAlpha(textPaint, opacity);
-                   let str = Buffer.contents(buffer);
-                   let tokens =
-                     str
-                     |> Revery.Font.shape(font)
-                     |> Revery.Font.ShapeResult.getGlyphStrings;
-                   List.iter(
-                     ((skiaFace, str)) => {
-                       Skia.Paint.setTypeface(textPaint, skiaFace);
+             let fgColor = getFgColor(cell);
 
-                       CanvasContext.drawText(
-                         ~paint=textPaint,
-                         ~x=float(startColumn) *. characterWidth,
-                         ~y=yOffset +. characterHeight +. lineSpacingOffset,
-                         ~text=str,
-                         canvasContext,
-                       );
-                     },
-                     tokens,
-                   );
-                 }),
-               );
+             let item =
+               TextAccumulator.{column, color: fgColor, uchar: cell.char};
+             accumulator := Accumulator.add(item, accumulator^);
+           };
+           Accumulator.flush(accumulator^)};
 
-             for (column in 0 to columns - 1) {
-               let cell = Screen.getCell(~row, ~column, screen);
+        let perLineRenderer =
+          ImmediateList.render(
+            ~scrollY,
+            ~rowHeight=lineHeight,
+            ~height=lineHeight *. float(rows),
+            ~count=rows,
+          );
 
-               let fgColor = getFgColor(cell);
+        perLineRenderer(~render=renderBackground, ());
+        perLineRenderer(~render=renderText, ());
 
-               let item =
-                 TextAccumulator.{column, color: fgColor, uchar: cell.char};
-               accumulator := Accumulator.add(item, accumulator^);
-             };
-             Accumulator.flush(accumulator^)};
+        // If the cursor is visible, let's paint it now
+        if (cursor.visible) {
+          let cursorColor =
+            defaultForeground
+            |> Option.value(~default=theme(15))
+            |> Revery.Color.toSkia;
 
-          let perLineRenderer =
-            ImmediateList.render(
-              ~scrollY,
-              ~rowHeight=lineHeight,
-              ~height=lineHeight *. float(rows),
-              ~count=rows,
-            );
+          let (yOffset, width, height) =
+            switch (cursor.shape) {
+            | BarLeft => (0., 2., lineHeight)
+            | Underline => (
+                lineHeight -. 2. -. lineSpacingOffset,
+                characterWidth,
+                2.,
+              )
+            | Unknown
+            | Block => (0., characterWidth, lineHeight)
+            };
 
-          perLineRenderer(~render=renderBackground, ());
-          perLineRenderer(~render=renderText, ());
-
-          // If the cursor is visible, let's paint it now
-          if (cursor.visible) {
-            let cursorColor =
-              defaultForeground
-              |> Option.value(~default=theme(15))
-              |> Revery.Color.toSkia;
-
-            let (yOffset, width, height) =
-              switch (cursor.shape) {
-              | BarLeft => (0., 2., lineHeight)
-              | Underline => (
-                  lineHeight -. 2. -. lineSpacingOffset,
-                  characterWidth,
-                  2.,
-                )
-              | Unknown
-              | Block => (0., characterWidth, lineHeight)
-              };
-
-            Skia.Paint.setColor(textPaint, cursorColor);
-            Skia.Paint.setAlpha(textPaint, opacity);
-            CanvasContext.drawRectLtwh(
-              ~paint=textPaint,
-              ~left=float(cursor.column) *. characterWidth,
-              ~top=
-                yOffset
-                +. float(scrollBackRows + cursor.row)
-                *. lineHeight
-                -. scrollY,
-              ~width,
-              ~height,
-              canvasContext,
-            );
-          };
-        }}
-      />
-      <View style={Styles.scrollBarContainer(~opacity)}>
-        <TerminalScrollBarView
-          onScroll
-          height={size.height}
-          width=scrollBarThickness
-          trackColor=scrollBarBackground
-          thumbColor=scrollBarThumb
-          scrollY
-          screen
-          font
-        />
-      </View>
-    </View>;
+          Skia.Paint.setColor(textPaint, cursorColor);
+          Skia.Paint.setAlpha(textPaint, opacity);
+          CanvasContext.drawRectLtwh(
+            ~paint=textPaint,
+            ~left=float(cursor.column) *. characterWidth,
+            ~top=
+              yOffset
+              +. float(scrollBackRows + cursor.row)
+              *. lineHeight
+              -. scrollY,
+            ~width,
+            ~height,
+            canvasContext,
+          );
+        };
+      }}
+    />;
 
   element;
 };

integration_test/TerminalConfigurationTest.re
@@ -64,9 +64,11 @@ runTest(
     let expectedTerminalExists = (state: State.t) => {
       state.terminals
       |> Feature_Terminal.toList
-      |> List.exists((terminal: Feature_Terminal.terminal) => {
-           terminal.launchConfig.executable == expectedShellCmd
-           && terminal.launchConfig.arguments == expectedShellArgs
+      |> List.exists((terminal: Feature_Terminal.Terminal.t) => {
+           let launchConfig =
+             Feature_Terminal.Terminal.launchConfig(terminal);
+           launchConfig.executable == expectedShellCmd
+           && launchConfig.arguments == expectedShellArgs;
          });
     };
 

integration_test/TerminalSetPidTitleTest.re
@@ -42,11 +42,15 @@ runTest(~name="TerminalSetPidTitle", ({dispatch, wait, _}) => {
     (state: State.t) =>
     switch (getTerminalOpt(state)) {
     | None => failwith("Terminal should be in state!")
-    | Some({id, pid, title, _}) =>
+    | Some(terminal: Feature_Terminal.Terminal.t) =>
       let logOpt = str =>
         str
         |> Option.map(str => "Some: " ++ str)
         |> Option.value(~default="None");
+
+      let id = Feature_Terminal.Terminal.id(terminal);
+      let pid = Feature_Terminal.Terminal.pid(terminal);
+      let title = Feature_Terminal.Terminal.title(terminal);
 
       prerr_endline(
         Printf.sprintf(

src/Components/Scrollbar.re
@@ -1,0 +1,64 @@
+open Revery.UI;
+
+module Colors = Feature_Theme.Colors;
+
+module Constants = {
+  let scrollBarThickness = 6;
+  let minimumThumbSize = 4;
+};
+
+module Styles = {
+  open Revery.UI.Style;
+  let slider = [
+    position(`Absolute),
+    right(0),
+    top(0),
+    bottom(0),
+    width(Constants.scrollBarThickness),
+    overflow(`Hidden),
+  ];
+};
+
+let make =
+    (
+      ~config,
+      ~pixelHeight,
+      ~viewportHeight,
+      ~totalHeight,
+      ~scrollY,
+      ~onScroll,
+      ~theme,
+      (),
+    ) => {
+  // TODO: Bring back scroll-speed
+  ignore(config);
+
+  let maxHeight = totalHeight -. viewportHeight;
+  let thumbHeight =
+    pixelHeight
+    *. viewportHeight
+    /. max(1., maxHeight)
+    |> max(float(Constants.minimumThumbSize));
+  let isVisible = maxHeight > 0.;
+
+  if (isVisible) {
+    <View style=Styles.slider>
+      <Revery.UI.Components.Slider
+        onValueChanged=onScroll
+        minimumValue=0.
+        maximumValue=maxHeight
+        sliderLength={int_of_float(pixelHeight)}
+        thumbLength={int_of_float(thumbHeight)}
+        value=scrollY
+        trackThickness=Constants.scrollBarThickness
+        thumbThickness=Constants.scrollBarThickness
+        minimumTrackColor=Revery.Colors.transparentBlack
+        maximumTrackColor=Revery.Colors.transparentBlack
+        thumbColor={Colors.ScrollbarSlider.activeBackground.from(theme)}
+        vertical=true
+      />
+    </View>;
+  } else {
+    React.empty;
+  };
+};

src/Feature/Terminal/Feature_Terminal.re
@@ -1,5 +1,7 @@
 open Oni_Core;
 
+module Terminal = Terminal;
+
 // MODEL
 include Model;
 
@@ -26,13 +28,53 @@ module Configuration = Configuration;
 
 let shouldClose = (~id, {idToTerminal, _}) => {
   IntMap.find_opt(id, idToTerminal)
-  |> Option.map(({closeOnExit, _}) => closeOnExit)
+  |> Option.map(Terminal.closeOnExit)
   |> Option.value(~default=true);
 };
 
 let updateById = (id, f, model) => {
   let idToTerminal = IntMap.update(id, Option.map(f), model.idToTerminal);
   {...model, idToTerminal};
+};
+
+let updateAll = (f, model) => {
+  let idToTerminal = IntMap.map(f, model.idToTerminal);
+  {...model, idToTerminal};
+};
+
+let recomputeFont = (~config, ~font: Service_Font.font, model) => {
+  let resolvedFont =
+    Service_Font.resolveWithFallback(
+      Revery.Font.Weight.Normal,
+      font.fontFamily,
+    );
+
+  let lineHeight =
+    Configuration.lineHeight.get(config)
+    |> Oni_Core.Utility.OptionEx.value_or_lazy(() => {
+         Feature_Configuration.GlobalConfiguration.Editor.lineHeight.get(
+           config,
+         )
+       });
+
+  let Service_Font.{fontSize, smoothing, _} = font;
+
+  let lineHeightSize =
+    Oni_Core.LineHeight.calculate(~measuredFontHeight=fontSize, lineHeight);
+  let resolvedFont =
+    EditorTerminal.Font.make(
+      ~smoothing,
+      ~size=fontSize,
+      ~lineHeight=lineHeightSize,
+      resolvedFont,
+    );
+
+  {...model, font, resolvedFont}
+  |> updateAll(Terminal.setFont(~font=resolvedFont));
+};
+
+let configurationChanged = (~config, model) => {
+  recomputeFont(~config, ~font=model.font, model);
 };
 
 let update =
@@ -43,7 +85,14 @@ let update =
       msg,
     ) => {
   switch (msg) {
-  | Font(Service_Font.FontLoaded(font)) => ({...model, font}, Nothing)
+  | Terminal({id, msg}) => (
+      model |> updateById(id, Terminal.update(msg)),
+      Nothing,
+    )
+
+  | Font(Service_Font.FontLoaded(font)) =>
+    let model' = model |> recomputeFont(~config, ~font);
+    (model', Nothing);
 
   | Font(Service_Font.FontLoadError(message)) => (
       model,
@@ -144,17 +193,7 @@ let update =
     let idToTerminal =
       IntMap.add(
         id,
-        {
-          id,
-          launchConfig,
-          rows: 40,
-          columns: 40,
-          pid: None,
-          title: None,
-          screen: EditorTerminal.Screen.initial,
-          cursor: EditorTerminal.Cursor.initial,
-          closeOnExit: true,
-        },
+        Terminal.initial(~font=model.resolvedFont, ~id, ~launchConfig, ()),
         model.idToTerminal,
       );
     (
@@ -254,17 +293,13 @@ let update =
     let idToTerminal =
       IntMap.add(
         id,
-        {
-          id,
-          launchConfig,
-          rows: 40,
-          columns: 40,
-          pid: None,
-          title: None,
-          screen: EditorTerminal.Screen.initial,
-          cursor: EditorTerminal.Cursor.initial,
-          closeOnExit,
-        },
+        Terminal.initial(
+          ~closeOnExit,
+          ~font=model.resolvedFont,
+          ~id,
+          ~launchConfig,
+          (),
+        ),
         model.idToTerminal,
       );
     (
@@ -289,19 +324,28 @@ let update =
            Effect(inputEffect);
          })
       |> Option.value(~default=Nothing);
-    (model, eff);
+
+    let model' =
+      model.paneTerminalId
+      |> Option.map(paneId =>
+           updateById(paneId, Terminal.scrollToBottom, model)
+         )
+      |> Option.value(~default=model);
+    (model', eff);
 
   | KeyPressed({id, key}) =>
     let inputEffect =
       Service_Terminal.Effect.input(~id, key)
       |> Isolinear.Effect.map(msg => Service(msg));
-    (model, Effect(inputEffect));
+    let model' = model |> updateById(id, Terminal.scrollToBottom);
+    (model', Effect(inputEffect));
 
   | Pasted({id, text}) =>
     let inputEffect =
       Service_Terminal.Effect.paste(~id, text)
       |> Isolinear.Effect.map(msg => Service(msg));
-    (model, Effect(inputEffect));
+    let model' = model |> updateById(id, Terminal.scrollToBottom);
+    (model', Effect(inputEffect));
 
   | Resized({id, rows, columns}) =>
     let newModel = updateById(id, term => {...term, rows, columns}, model);
@@ -317,7 +361,8 @@ let update =
     (newModel, Nothing);
 
   | Service(ScreenUpdated({id, screen, cursor})) =>
-    let newModel = updateById(id, term => {...term, screen, cursor}, model);
+    let newModel =
+      updateById(id, Terminal.updateScreen(~screen, ~cursor), model);
     (newModel, Nothing);
 
   | Service(ProcessExit({id, exitCode})) =>
@@ -798,7 +843,6 @@ module Contributions = {
                    isActive=isFocused
                    config
                    terminal
-                   font={model.font}
                    theme
                    dispatch
                  />

src/Feature/Terminal/Feature_Terminal.rei
@@ -2,29 +2,27 @@ open Oni_Core;
 
 // MODEL
 
-type terminal =
-  pri {
-    id: int,
-    launchConfig: Exthost.ShellLaunchConfig.t,
-    rows: int,
-    columns: int,
-    pid: option(int),
-    title: option(string),
-    screen: EditorTerminal.Screen.t,
-    cursor: EditorTerminal.Cursor.t,
-    closeOnExit: bool,
-  };
+module Terminal: {
+  type t;
+
+  let id: t => int;
+  let pid: t => option(int);
+  let title: t => option(string);
+  let launchConfig: t => Exthost.ShellLaunchConfig.t;
+};
 
 type t;
 
 let initial: t;
 
-let toList: t => list(terminal);
+let toList: t => list(Terminal.t);
 
-let getTerminalOpt: (int, t) => option(terminal);
+let getTerminalOpt: (int, t) => option(Terminal.t);
 
 // Font to be used for terminals
 let font: t => Service_Font.font;
+
+let configurationChanged: (~config: Config.resolver, t) => t;
 
 // UPDATE
 
@@ -161,7 +159,6 @@ module TerminalView: {
       ~config: Config.resolver,
       ~id: int,
       ~terminals: t,
-      ~font: Service_Font.font,
       ~theme: Oni_Core.ColorTheme.Colors.t,
       ~dispatch: msg => unit,
       unit

src/Feature/Terminal/Model.re
@@ -2,23 +2,14 @@ open Oni_Core;
 
 // MODEL
 
-type terminal = {
-  id: int,
-  launchConfig: Exthost.ShellLaunchConfig.t,
-  rows: int,
-  columns: int,
-  pid: option(int),
-  title: option(string),
-  screen: EditorTerminal.Screen.t,
-  cursor: EditorTerminal.Cursor.t,
-  closeOnExit: bool,
-};
+type terminal = Terminal.t;
 
 type t = {
   idToTerminal: IntMap.t(terminal),
   nextId: int,
   paneTerminalId: option(int),
   font: Service_Font.font,
+  resolvedFont: EditorTerminal.Font.t,
 };
 
 let font = ({font, _}) => font;
@@ -27,6 +18,15 @@ let initial = {
   nextId: 0,
   paneTerminalId: None,
   font: Service_Font.default(),
+  resolvedFont:
+    EditorTerminal.Font.make(
+      ~size=14.0,
+      ~lineHeight=14.0,
+      Service_Font.resolveWithFallback(
+        Revery.Font.Weight.Normal,
+        Service_Font.default().fontFamily,
+      ),
+    ),
 };
 
 let getBufferName = (id, cmd) =>
@@ -54,6 +54,10 @@ type command =
 [@deriving show({with_path: false})]
 type msg =
   | Command(command)
+  | Terminal({
+      id: int,
+      msg: Terminal.msg,
+    })
   | Font(Service_Font.msg)
   | Resized({
       id: int,

commit 4a981dd03cfa7e018865c66a325aff13df261010

fix(terminal/#3711): Normalize scroll direction from mouse wheel (#3717)
__Issue:__ When scrolling using the mousewheel / trackpad in terminal, the direction is the opposite the expected.

__Defect:__ Regression from https://github.com/onivim/oni2/pull/3645 - when refactoring to track the scroll in the model, the mousewheel direction wasn't normalized like it was in the editor/scrollable lists.

__Fix:__ Normalize mouse-wheel factor so the scroll direction is correct and consistent with the rest of the UI.

Fixes #3711

src/Feature/Terminal/TerminalView.re
@@ -98,7 +98,7 @@ module Terminal = {
       dispatch(
         Terminal({
           id: terminal.id,
-          msg: MouseWheelScrolled({deltaY: deltaY *. 25.0}),
+          msg: MouseWheelScrolled({deltaY: deltaY *. (-25.0)}),
         }),
       );
     };